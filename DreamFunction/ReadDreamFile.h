393bed9eed4844970a4dc939fb62ff89fab93546
Andreas Mathis <andreas.mathis@cern.ch>
2018-09-18 09:56:49 +0200

    Include mT analysis

--- DreamFunction/ReadDreamFile.h
+++ DreamFunction/ReadDreamFile.h
@@ -19,17 +19,21 @@ class ReadDreamFile {
   virtual ~ReadDreamFile();
   void SetAnalysisFile(const char* AnalysisFile, const char* prefix, const char* Addon="");
   void ReadkTHistos(const char* AnalysisFile, const char* prefix, const char* addon = "");
+  void ReadmTHistos(const char* AnalysisFile, const char* prefix, const char* addon = "");
   DreamDist* GetPairDistributions(int iPart1, int iPart2, const char* name);
   DreamKayTee* GetkTPairDistributions(int iPart1, int iPart2,int iAPart1, int iAPart2);
+  DreamKayTee* GetmTPairDistributions(int iPart1, int iPart2,int iAPart1, int iAPart2);
   const int fNPart1;
   const int fNPart2;
  private:
   TH1F*** fSE;
   TH2F*** fSEMult;
   TH2F*** fSEkT;
+  TH2F*** fSEmT;
   TH1F*** fME;
   TH2F*** fMEMult;
   TH2F*** fMEkT;
+  TH2F*** fMEmT;
 };
 
 #endif /* DREAMFUNCTION_READDREAMFILE_H_ */
