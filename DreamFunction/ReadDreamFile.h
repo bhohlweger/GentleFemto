7495ecbcf6c0778f9125143217db95aa1f349b6d
Andreas Mathis <andreas.mathis@cern.ch>
2018-10-22 15:26:38 +0200

    The Sigma0 joins the dream

--- DreamFunction/ReadDreamFile.h
+++ DreamFunction/ReadDreamFile.h
@@ -18,6 +18,7 @@ class ReadDreamFile {
   ReadDreamFile(int nPart1, int nPart2);
   virtual ~ReadDreamFile();
   void SetAnalysisFile(const char* AnalysisFile, const char* prefix, const char* Addon="");
+  void SetSigmaAnalysisFile(const char* PathAnalysisFile, const char* suffix);
   void ExtractResults(const TList *Results);
   void ReadkTHistos(const char* AnalysisFile, const char* prefix, const char* addon = "");
   void ReadmTHistos(const char* AnalysisFile, const char* prefix, const char* addon = "");
