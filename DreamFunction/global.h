9c1a61324bc8532b2fd10b9bcb59f9431364c07a
Andreas Mathis <andreas.mathis@cern.ch>
2018-11-13 17:26:27 +0100

    Remove baseline from global file

--- DreamFunction/global.h
+++ DreamFunction/global.h
@@ -12,11 +12,5 @@ extern int globalBinWidth;  // in MeV
 extern const char* globalCollisionSystem;
 extern const char* globalEventGenerator;
 extern float globalBeamEnergy;  // TeV
-extern float globalBaselinePL0;
-extern float globalBaselinePL1;
-extern float globalBaselineLL0;
-extern float globalBaselineLL1;
-extern float globalBaselinePXi0;
-extern float globalBaselinePXi1;
 
 #endif
