850e01c590b330b415b16d13bd61092920f1b09f
Andreas Mathis <andreas.mathis@cern.ch>
2019-01-10 14:46:57 +0100

    Write out the best/def/worst chi2 in exclusion mode

--- GentleKitty/Scripts/DrawSigma.C
+++ GentleKitty/Scripts/DrawSigma.C
@@ -18,6 +18,37 @@
 
 const int tupleLength = 14;
 
+void ComputeChi2(TH1F* dataHist, TGraphErrors *grFit, double &bestChi2,
+                 double &defaultChi2, double &worstChi2) {
+  double theoryX, theoryY, theoryErr;
+  double dataY, dataErr, dataErrSq;
+  double chi2Best, chi2Worst;
+  double chi2Down, chi2Def, chi2Up;
+  int maxkStarBinChi2 = dataHist->FindBin(250);
+  for (unsigned uBin = 1; uBin <= maxkStarBinChi2; uBin++) {
+    double mom = dataHist->GetBinCenter(uBin);
+    grFit->GetPoint(uBin - 1, theoryX, theoryY);
+    theoryErr = grFit->GetErrorY(uBin - 1);
+
+    if (mom != theoryX) {
+      std::cerr << "PROBLEM Sigma0 " << mom << '\t' << theoryX << std::endl;
+    }
+    dataY = dataHist->GetBinContent(uBin);
+    dataErr = dataHist->GetBinError(uBin);
+    dataErrSq = (dataErr * dataErr);
+
+    chi2Up += (dataY - (theoryY + theoryErr)) * (dataY - (theoryY + theoryErr))
+        / dataErrSq;
+    chi2Def += (dataY - theoryY) * (dataY - theoryY) / dataErrSq;
+    chi2Down += (dataY - (theoryY - theoryErr))
+        * (dataY - (theoryY - theoryErr)) / dataErrSq;
+  }
+  worstChi2 = (chi2Up > chi2Down) ? chi2Up : chi2Down;
+  defaultChi2 = chi2Def;
+  bestChi2 = (chi2Up > chi2Down) ? chi2Down : chi2Up;
+
+}
+
 // =========================================
 // Compute per k* bin the 68% variations
 void EvalError(TNtuple *tuple, const int iBranches, TH1F* histCF,
@@ -43,7 +74,9 @@ void EvalError(TNtuple *tuple, const int iBranches, TH1F* histCF,
 
 // =========================================
 // Draw all systematic variations available
-void DrawSigma(TString InputDir, TString appendix, TString varFolder) {
+void DrawSigma(const unsigned& NumIter, TString InputDir, TString appendix,
+               TString varFolder, const bool& isExclusion, const float d0,
+               const float f0inv) {
   DreamPlot::SetStyle();
   CATSInputSigma0 *CATSinput = new CATSInputSigma0();
   CATSinput->ReadSigma0CorrelationFile(InputDir.Data(), appendix.Data());
@@ -54,12 +87,16 @@ void DrawSigma(TString InputDir, TString appendix, TString varFolder) {
   CF_Histo->GetYaxis()->SetRangeUser(0.8, 1.6);
   CF_Histo->SetTitle("; #it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
   DreamPlot::SetStyleHisto(CF_Histo, 24, kBlack);
-  const char* beg = "Param";
-  const char* ext = ".root";
 
-  char* dir = gSystem->ExpandPathName(varFolder.Data());
-  void* dirp = gSystem->OpenDirectory(dir);
-  TString MygraphName = "";
+  TString graphfilename;
+  if (isExclusion) {
+    graphfilename = TString::Format("%s/Param_pSigma0_%i_%f_%f.root",
+                                    varFolder.Data(), NumIter, d0, f0inv);
+  } else {
+    graphfilename = TString::Format("%s/Param_pSigma0_%i.root",
+                                    varFolder.Data(), NumIter);
+
+  }
 
   float tupler[tupleLength];
   auto fit = new TNtuple("fit", "fit",
@@ -70,56 +107,46 @@ void DrawSigma(TString InputDir, TString appendix, TString varFolder) {
                               "delta1:delta2:delta3:delta4:delta5:delta6:"
                               "delta7:delta8:delta9:delta10:delta11:"
                               "delta12:delta13:delta14");
-  int counter = 0;
-  const char* entry;
   std::vector<TGraph*> Graph;
-  TFile *file;
-  Int_t n = 0;
-  TString str;
+  auto file = TFile::Open(graphfilename.Data(), "update");
   auto c1 = new TCanvas("CF_var", "CF_var");
   CF_Histo->Draw();
-  while ((entry = (char*) gSystem->GetDirEntry(dirp))) {
-    str = entry;
-    if (str.EndsWith(ext) && str.BeginsWith(beg)) {
-      file = TFile::Open(gSystem->ConcatFileName(dir, entry), "update");
-      TIter next(file->GetListOfKeys());
-      TKey *key;
-      while ((key = (TKey*) next())) {
-        TClass *cl = gROOT->GetClass(key->GetClassName());
-        if (!cl->InheritsFrom("TDirectoryFile")) {
-          continue;
+  TIter next(file->GetListOfKeys());
+  TKey *key;
+  while ((key = (TKey*) next())) {
+    TClass *cl = gROOT->GetClass(key->GetClassName());
+    if (!cl->InheritsFrom("TDirectoryFile")) {
+      continue;
+    }
+    auto dirFile = (TDirectoryFile*) key->ReadObj();
+    TIter nextnext(dirFile->GetListOfKeys());
+    TKey *nextkey;
+    while ((nextkey = (TKey*) nextnext())) {
+
+      auto Graph = (TGraph*) nextkey->ReadObj();
+      TString graphName = Form("%s", Graph->GetName());
+
+      if (graphName.Contains("Sigma0Sideband")) {
+        c1->cd();
+        Graph->SetLineColor(kGray + 2);
+        Graph->SetLineStyle(0);
+        Graph->Draw("L3same");
+        for (int iPnt = 0; iPnt < tupleLength; ++iPnt) {
+          tupler[iPnt] = CF_Histo->GetBinContent(iPnt + 1)
+              - Graph->Eval(CF_Histo->GetBinCenter(iPnt + 1));
         }
-        auto dirFile = (TDirectoryFile*) key->ReadObj();
-        TIter nextnext(dirFile->GetListOfKeys());
-        TKey *nextkey;
-        while ((nextkey = (TKey*) nextnext())) {
+        sideband->Fill(tupler);
+      }
 
-          auto Graph = (TGraph*) nextkey->ReadObj();
-          TString graphName = Form("%s", Graph->GetName());
-
-          if (graphName.Contains("Sigma0Sideband")) {
-            c1->cd();
-            Graph->SetLineColor(kGray + 2);
-            Graph->SetLineStyle(0);
-            Graph->Draw("L3same");
-            for (int iPnt = 0; iPnt < tupleLength; ++iPnt) {
-              tupler[iPnt] = CF_Histo->GetBinContent(iPnt + 1)
-                  - Graph->Eval(CF_Histo->GetBinCenter(iPnt + 1));
-            }
-            sideband->Fill(tupler);
-          }
-
-          if (graphName.Contains("pSigma0Graph")) {
-            c1->cd();
-            Graph->SetLineColor(kRed + 2);
-            Graph->Draw("L3same");
-            for (int iPnt = 0; iPnt < tupleLength; ++iPnt) {
-              tupler[iPnt] = CF_Histo->GetBinContent(iPnt + 1)
-                  - Graph->Eval(CF_Histo->GetBinCenter(iPnt + 1));
-            }
-            fit->Fill(tupler);
-          }
+      if (graphName.Contains("pSigma0Graph")) {
+        c1->cd();
+        Graph->SetLineColor(kRed + 2);
+        Graph->Draw("L3same");
+        for (int iPnt = 0; iPnt < tupleLength; ++iPnt) {
+          tupler[iPnt] = CF_Histo->GetBinContent(iPnt + 1)
+              - Graph->Eval(CF_Histo->GetBinCenter(iPnt + 1));
         }
+        fit->Fill(tupler);
       }
     }
   }
@@ -143,6 +170,34 @@ void DrawSigma(TString InputDir, TString appendix, TString varFolder) {
     EvalError(sideband, iBranches, CF_Histo, grSidebands);
   }
 
+  // in case we're running the exclusion task, we're interested in the best/worst chi2 for a given set of scattering parameters
+  if (isExclusion) {
+    auto fitTuple = (TNtuple*) file->Get("fitResult");
+
+    TString resultfilename = TString::Format("%s/Result_%f_%f.root",
+                                             varFolder.Data(), d0, f0inv);
+    auto resultFile = TFile::Open(resultfilename.Data(), "recreate");
+    TNtuple* ntResult = new TNtuple(
+        "exclusion", "exclusion", "CFneg:d0:f0inv:bestChi2:defChi2:worstChi2");
+    double bestChi2, defaultChi2, worstChi2;
+    ComputeChi2(CF_Histo, grCF, bestChi2, defaultChi2, worstChi2);
+
+    Float_t ntBuffer[6];
+    fitTuple->Draw("CFneg >> h");
+    TH1F* hist = (TH1F*) gROOT->FindObject("h");
+    ntBuffer[0] = hist->GetMean();
+    ntBuffer[1] = d0;
+    ntBuffer[2] = f0inv;
+    ntBuffer[3] = bestChi2;
+    ntBuffer[4] = defaultChi2;
+    ntBuffer[5] = worstChi2;
+
+    ntResult->Fill(ntBuffer);
+    ntResult->Write();
+    resultFile->Close();
+  }
+
+  file->cd();
   auto c = new TCanvas("pSigma0Correlation");
   auto histDummy = new TH1F("histDummy",
                             "; #it{k}* (MeV/#it{c}); #it{C}(#it{k}*)", 100, 0,
@@ -161,13 +216,13 @@ void DrawSigma(TString InputDir, TString appendix, TString varFolder) {
   BeamText.SetNDC(kTRUE);
   BeamText.DrawLatex(0.525, 0.8, "ALICE ");
   BeamText.DrawLatex(0.525, 0.75, "pp #sqrt{s} = 13 TeV HM");
-  auto leg = new TLegend(0.51,0.53,0.85,0.73);
+  auto leg = new TLegend(0.51, 0.53, 0.85, 0.73);
   leg->SetBorderSize(0);
   leg->SetTextFont(42);
   leg->SetTextSize(gStyle->GetTextSize());
   leg->AddEntry(CF_Histo, "p#Sigma^{0} #oplus #bar{p}#bar{#Sigma}^{0}", "pe");
-  leg->AddEntry(grCF,"Femtoscopic fit","f");
-  leg->AddEntry(grSidebands,"Background","f");
+  leg->AddEntry(grCF, "Femtoscopic fit", "f");
+  leg->AddEntry(grSidebands, "Background", "f");
   leg->Draw("same");
 
   c->SaveAs(Form("%s/CF_pSigma_fit.pdf", varFolder.Data()));
@@ -180,6 +235,7 @@ void DrawSigma(TString InputDir, TString appendix, TString varFolder) {
 // =========================================
 // main
 int main(int argc, char *argv[]) {
-  DrawSigma(argv[1], argv[2], argv[3]);
+  DrawSigma(atoi(argv[1]), argv[2], argv[3], argv[4], atoi(argv[5]),
+            atof(argv[6]), atof(argv[7]));
   return 0;
 }
