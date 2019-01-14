32142e47dc139abda99040bb89ac492fde866bd1
bhohlweger <bernhard.hohlweger@cern.ch>
2019-01-14 15:36:03 +0100

    fix the correct weights of the isoSpin channels and add a cut off function

--- GentleKitty/TidyCats.cxx
+++ GentleKitty/TidyCats.cxx
@@ -125,10 +125,10 @@ void TidyCats::GetCatsProtonXiMinus(CATS* AB_pXim, double* pars, int momBins,
   AB_pXim->SetSpin(1, 1);    //I=0; S=1
   AB_pXim->SetSpin(2, 0);    //I=1; S=0
   AB_pXim->SetSpin(3, 1);    //I=1; S=1
-  AB_pXim->SetChannelWeight(0, 1. / 4.);
-  AB_pXim->SetChannelWeight(1, 3. / 4.);
-  AB_pXim->SetChannelWeight(2, 1. / 4.);
-  AB_pXim->SetChannelWeight(3, 3. / 4.);
+  AB_pXim->SetChannelWeight(0, 1. / 8.);
+  AB_pXim->SetChannelWeight(1, 3. / 8.);
+  AB_pXim->SetChannelWeight(2, 1. / 8.);
+  AB_pXim->SetChannelWeight(3, 3. / 8.);
 
   AB_pXim->SetQ1Q2(-1);
 
@@ -138,14 +138,70 @@ void TidyCats::GetCatsProtonXiMinus(CATS* AB_pXim, double* pars, int momBins,
   AB_pXim->SetMaxRad(64);
   AB_pXim->SetMaxRho(32);
 //
-  static double pXimPotParsI0S0[10] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1,
+  static double pXimPotParsI0S0[11] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1,
       0, 0, 0 };  //4th argument is the t parameter and can be:
-  static double pXimPotParsI0S1[10] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1,
+  static double pXimPotParsI0S1[11] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1,
       1, 0, 1 };  // 9, 10, 11, 12
-  static double pXimPotParsI1S0[10] = { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 0,
+  static double pXimPotParsI1S0[11] = { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 0,
       0, 0 };  //This is shit. Corresponds to 9-14 t
-  static double pXimPotParsI1S1[10] = { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 1,
+  static double pXimPotParsI1S1[11] = { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 1,
       0, 1 };  // this value 1-6
+  if (StrongOn) {
+    AB_pXim->SetShortRangePotential(0, 0, fDlmPot, pXimPotParsI0S0);
+    AB_pXim->SetShortRangePotential(1, 0, fDlmPot, pXimPotParsI0S1);
+    AB_pXim->SetShortRangePotential(2, 0, fDlmPot, pXimPotParsI1S0);
+    AB_pXim->SetShortRangePotential(3, 0, fDlmPot, pXimPotParsI1S1);
+  }
+
+  return;
+}
+
+void TidyCats::GetCatsProtonXiMinusCutOff(CATS* AB_pXim, double* pars, int momBins,
+                                    double kMin, double kMax, bool StrongOn,
+                                    double QCDTime, double cutOff) {
+
+  const double Mass_p = TDatabasePDG::Instance()->GetParticle(2212)->Mass()
+      * 1000;
+  const double Mass_Xim = TDatabasePDG::Instance()->GetParticle(3312)->Mass()
+      * 1000;
+
+  AB_pXim->SetAnaSource(GaussSource, pars);
+  AB_pXim->SetUseAnalyticSource(true);
+  AB_pXim->SetThetaDependentSource(false);
+
+  AB_pXim->SetExcludeFailedBins(false);
+  AB_pXim->SetMomBins(momBins, kMin, kMax);
+
+  AB_pXim->SetNumChannels(4);
+  AB_pXim->SetNumPW(0, 1);
+  AB_pXim->SetNumPW(1, 1);
+  AB_pXim->SetNumPW(2, 1);
+  AB_pXim->SetNumPW(3, 1);
+  AB_pXim->SetSpin(0, 0);    //I=0; S=0
+  AB_pXim->SetSpin(1, 1);    //I=0; S=1
+  AB_pXim->SetSpin(2, 0);    //I=1; S=0
+  AB_pXim->SetSpin(3, 1);    //I=1; S=1
+  AB_pXim->SetChannelWeight(0, 1. / 8.);
+  AB_pXim->SetChannelWeight(1, 3. / 8.);
+  AB_pXim->SetChannelWeight(2, 1. / 8.);
+  AB_pXim->SetChannelWeight(3, 3. / 8.);
+
+  AB_pXim->SetQ1Q2(-1);
+
+  AB_pXim->SetPdgId(2212, 3122);  //same as Lambda, in case we want to use EPOS pL source
+
+  AB_pXim->SetRedMass((Mass_p * Mass_Xim) / (Mass_p + Mass_Xim));
+  AB_pXim->SetMaxRad(64);
+  AB_pXim->SetMaxRho(32);
+//
+  static double pXimPotParsI0S0[11] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1,
+      0, 0, 0, cutOff };  //4th argument is the t parameter and can be:
+  static double pXimPotParsI0S1[11] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1,
+      1, 0, 1, cutOff };  // 9, 10, 11, 12
+  static double pXimPotParsI1S0[11] = { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 0,
+      0, 0, cutOff };  //This is shit. Corresponds to 9-14 t
+  static double pXimPotParsI1S1[11] = { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 1,
+      0, 1, cutOff  };  // this value 1-6
   if (StrongOn) {
     AB_pXim->SetShortRangePotential(0, 0, fDlmPot, pXimPotParsI0S0);
     AB_pXim->SetShortRangePotential(1, 0, fDlmPot, pXimPotParsI0S1);
