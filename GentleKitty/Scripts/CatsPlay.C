636947ff7963dce481aacf35d9c1fe5bf0db762b
bhohlweger <bernhard.hohlweger@cern.ch>
2019-01-14 09:41:06 +0100

    Commented out the wave function since cats bug was fixed

--- GentleKitty/Scripts/CatsPlay.C
+++ GentleKitty/Scripts/CatsPlay.C
@@ -28,15 +28,19 @@ void PlotPotentials() {
   TidyCats* tidy = new TidyCats();
   CATSInput *CATSinput = new CATSInput();
 //  std::vector<float> cutOffValues = { 0. };
-  std::vector<float> cutOffValues = { 0., 0.1, 0.3, 0.5, 0.7, 0.8, 0.9, 1.0,
-      1.1, 1.3, 1.5, 2.0, 2.5, 3.0};
+  std::vector<float> cutOffValues = { 0., 0.1, 0.3, 0.5, 0.55, 0.6, 0.65, 0.7, 0.8, 0.9, 1.0,
+      1.1, 1.3, 1.5, 2.0};
 //  std::vector<float> cutOffValues = { 0.3, 0.7, 1.0, 1.02, 1.06, 1.1, 1.3, 5.0 };
   const int nCutOffs = cutOffValues.size();
   TH1F* CF_I0S0[nCutOffs];
+//  TH2F* CF_I0S0WaveFunction[nCutOffs];
   TH1F* CF_I0S1[nCutOffs];
+//  TH2F* CF_I0S1WaveFunction[nCutOffs];
   TH1F* CF_I1S0[nCutOffs];
+//  TH2F* CF_I1S0WaveFunction[nCutOffs];
   TH1F* CF_I1S1[nCutOffs];
-  TLine one = TLine(0., 1., 200., 1.);
+//  TH2F* CF_I1S1WaveFunction[nCutOffs];
+  TLine one = TLine(0., 1., 400., 1.);
   double QCDTime = 11;
 //4th argument is the t parameter and can be:
 // 9, 10, 11, 12
@@ -45,13 +49,19 @@ void PlotPotentials() {
       * 1000;
   const double Mass_Xim = TDatabasePDG::Instance()->GetParticle(3312)->Mass()
       * 1000;
-  int momBins = 44;
+  int RadBins = 1000;
+  float rMin = 0.;
+  float rMax = 10.;
+  float dRad = (rMax - rMin)/(float)RadBins;
+  int momBins = 80;
   float kMin = 0.;
-  float kMax = 220;
+  float kMax = 400;
   int iCutOff = 0;
   TFile* outfile = TFile::Open("output.root", "recreate");
   auto c1 = new TCanvas("c1", "c1");
-  c1->Divide(2, 2);
+  auto c2 = new TCanvas("c2", "c2");
+  auto c3 = new TCanvas("c3", "c3");
+  auto c4 = new TCanvas("c4", "c4");
   TLegend leg(0.7, 0.5, 0.9, 0.9);
   TList *ListI0S0 = new TList();
   ListI0S0->SetOwner();
@@ -75,14 +85,14 @@ void PlotPotentials() {
       COLORMEBLIND -= 99;
     }
 //    int COLORMEBLIND = 1 + iCutOff;
-    double pXimPotParsI0S0[10] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1, 0, 0,
-        0 };
-    double pXimPotParsI0S1[10] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1, 1, 0,
-        1 };
-    double pXimPotParsI1S0[10] =
-        { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 0, 0, 0 };
-    double pXimPotParsI1S1[10] =
-        { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 1, 0, 1 };
+    double pXimPotParsI0S0[11] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1, 0, 0,
+        0, it };
+    double pXimPotParsI0S1[11] = { 0, 0, pXim_HALQCD1, QCDTime, 0, -1, 1, 1, 0,
+        1, it };
+    double pXimPotParsI1S0[11] =
+        { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 0, 0, 0, it };
+    double pXimPotParsI1S1[11] =
+        { 0, 0, pXim_HALQCD1, QCDTime, 1, 1, 1, 1, 0, 1, it };
 //    static double pXimPotParsI0S0[10] = { 0, 0, NN_AV18, v18_Coupled3P2, 1, 1, 1, 0, 0,
 //        0 };
 //    static double pXimPotParsI0S1[10] = { 0, 0, NN_AV18, v18_Coupled3P2, 1, 1, 1, 1, 1,
@@ -96,18 +106,34 @@ void PlotPotentials() {
     TString I0S0HistTitle = Form("I0S0");
     CF_I0S0[iCutOff] = new TH1F(I0S0HistName.Data(), I0S0HistTitle.Data(),
                                 momBins, kMin, kMax);
+//    I0S0HistName+="WF";
+//    CF_I0S0WaveFunction[iCutOff] = new TH2F(I0S0HistName.Data(), I0S0HistTitle.Data(),
+//                                momBins, kMin, kMax,RadBins,rMin,rMax);
+
     TString I0S1HistName = Form("hI0S1_%.2f", it);
     TString I0S1HistTitle = Form("I0S1");
     CF_I0S1[iCutOff] = new TH1F(I0S1HistName.Data(), I0S1HistTitle.Data(),
                                 momBins, kMin, kMax);
+//    I0S1HistName+="WF";
+//    CF_I0S1WaveFunction[iCutOff] = new TH2F(I0S1HistName.Data(), I0S1HistTitle.Data(),
+//                                momBins, kMin, kMax,RadBins,rMin,rMax);
+
     TString I1S0HistName = Form("hI1S0_%.2f", it);
     TString I1S0HistTitle = Form("I1S0");
     CF_I1S0[iCutOff] = new TH1F(I1S0HistName.Data(), I1S0HistTitle.Data(),
                                 momBins, kMin, kMax);
+//    I1S0HistName+="WF";
+//    CF_I1S0WaveFunction[iCutOff] = new TH2F(I1S0HistName.Data(), I1S0HistTitle.Data(),
+//                                momBins, kMin, kMax,RadBins,rMin,rMax);
+
     TString I1S1HistName = Form("hI1S1_%.2f", it);
     TString I1S1HistTitle = Form("I1S1");
     CF_I1S1[iCutOff] = new TH1F(I1S1HistName.Data(), I1S1HistTitle.Data(),
                                 momBins, kMin, kMax);
+//    I1S1HistName+="WF";
+//    CF_I1S1WaveFunction[iCutOff] = new TH2F(I1S1HistName.Data(), I1S1HistTitle.Data(),
+//                                momBins, kMin, kMax,RadBins,rMin,rMax);
+
     double GaussSourceSize = 1.4;
 
     CATS AB_pXim_I0S0;
@@ -202,10 +228,21 @@ void PlotPotentials() {
       CF_I0S1[iCutOff]->SetBinContent(ikS + 1, AB_pXim_I0S1.GetCorrFun(ikS));
       CF_I1S0[iCutOff]->SetBinContent(ikS + 1, AB_pXim_I1S0.GetCorrFun(ikS));
       CF_I1S1[iCutOff]->SetBinContent(ikS + 1, AB_pXim_I1S1.GetCorrFun(ikS));
+
+//      for (int iRad = 0; iRad < RadBins; ++iRad) {
+//        complex<double> I0S0WF = AB_pXim_I0S0.EvalRadialWaveFunction(ikS,0,0,dRad*iRad,false);
+//        CF_I0S0WaveFunction[iCutOff]->SetBinContent(ikS+1,iRad+1,norm(I0S0WF));
+//        complex<double> I0S1WF = AB_pXim_I0S1.EvalRadialWaveFunction(ikS,0,0,dRad*iRad,false);
+//        CF_I0S1WaveFunction[iCutOff]->SetBinContent(ikS+1,iRad+1,norm(I0S1WF));
+//        complex<double> I1S0WF = AB_pXim_I1S0.EvalRadialWaveFunction(ikS,0,0,dRad*iRad,false);
+//        CF_I1S0WaveFunction[iCutOff]->SetBinContent(ikS+1,iRad+1,norm(I1S0WF));
+//        complex<double> I1S1WF = AB_pXim_I1S1.EvalRadialWaveFunction(ikS,0,0,dRad*iRad,false);
+//        CF_I1S1WaveFunction[iCutOff]->SetBinContent(ikS+1,iRad+1,norm(I1S1WF));
+//      }
     }
-    c1->cd(1);
+    c1->cd();
 //    CF_I0S0[iCutOff]->GetYaxis()->SetRangeUser(0.8, 3.5);
-    CF_I0S0[iCutOff]->GetXaxis()->SetRangeUser(0., 200.);
+    CF_I0S0[iCutOff]->GetXaxis()->SetRangeUser(0., 400.);
     CF_I0S0[iCutOff]->SetStats(0);
     CF_I0S0[iCutOff]->SetLineWidth(1);
     CF_I0S0[iCutOff]->SetLineColor(COLORMEBLIND);
@@ -220,10 +257,11 @@ void PlotPotentials() {
     }
     outfile->cd();
     ListI0S0->Add(CF_I0S0[iCutOff]);
+//    ListI0S0->Add(CF_I0S0WaveFunction[iCutOff]);
 
-    c1->cd(2);
-//    CF_I0S1[iCutOff]->GetYaxis()->SetRangeUser(0.95, 2);
-    CF_I0S1[iCutOff]->GetXaxis()->SetRangeUser(0., 200.);
+    c2->cd();
+    CF_I0S1[iCutOff]->GetYaxis()->SetRangeUser(0.95, 1.8);
+    CF_I0S1[iCutOff]->GetXaxis()->SetRangeUser(0., 400.);
     CF_I0S1[iCutOff]->SetStats(0);
     CF_I0S1[iCutOff]->SetLineWidth(1);
     CF_I0S1[iCutOff]->SetLineColor(COLORMEBLIND);
@@ -239,10 +277,11 @@ void PlotPotentials() {
 
     outfile->cd();
     ListI0S1->Add(CF_I0S1[iCutOff]);
+//    ListI0S1->Add(CF_I0S1WaveFunction[iCutOff]);
 
-    c1->cd(3);
-//    CF_I1S0[iCutOff]->GetYaxis()->SetRangeUser(0.95, 1.4);
-    CF_I1S0[iCutOff]->GetXaxis()->SetRangeUser(0., 200.);
+    c3->cd();
+    CF_I1S0[iCutOff]->GetYaxis()->SetRangeUser(0.95, 1.4);
+    CF_I1S0[iCutOff]->GetXaxis()->SetRangeUser(0., 400.);
     CF_I1S0[iCutOff]->SetStats(0);
     CF_I1S0[iCutOff]->SetLineWidth(1);
     CF_I1S0[iCutOff]->SetLineColor(COLORMEBLIND);
@@ -257,10 +296,11 @@ void PlotPotentials() {
     }
     outfile->cd();
     ListI1S0->Add(CF_I1S0[iCutOff]);
+//    ListI1S0->Add(CF_I1S0WaveFunction[iCutOff]);
 
-    c1->cd(4);
-//    CF_I1S1[iCutOff]->GetYaxis()->SetRangeUser(0.95, 1.5);
-    CF_I1S1[iCutOff]->GetXaxis()->SetRangeUser(0., 200.);
+    c4->cd();
+    CF_I1S1[iCutOff]->GetYaxis()->SetRangeUser(0.95, 1.5);
+    CF_I1S1[iCutOff]->GetXaxis()->SetRangeUser(0., 400.);
     CF_I1S1[iCutOff]->SetStats(0);
     CF_I1S1[iCutOff]->SetLineWidth(1);
     CF_I1S1[iCutOff]->SetLineColor(COLORMEBLIND);
@@ -275,23 +315,30 @@ void PlotPotentials() {
     }
     outfile->cd();
     ListI1S1->Add(CF_I1S1[iCutOff]);
+//    ListI1S1->Add(CF_I1S1WaveFunction[iCutOff]);
 
-    leg.AddEntry(CF_I0S1[iCutOff], TString::Format("Iteration %u", iCutOff),
+    leg.AddEntry(CF_I0S1[iCutOff], TString::Format("Cut off %.2f", it),
                  "lp");
 
     iCutOff++;
   }
   outfile->cd();
-  c1->cd(1);
+  c1->cd();
   leg.Draw("same");
-  c1->cd(2);
+  c2->cd();
   leg.Draw("same");
-  c1->cd(3);
+  c3->cd();
   leg.Draw("same");
-  c1->cd(4);
+  c4->cd();
   leg.Draw("same");
   c1->Write();
-  c1->SaveAs("Frezze.pdf");
+  c2->Write();
+  c3->Write();
+  c4->Write();
+  c1->SaveAs("I0S0.pdf");
+  c2->SaveAs("I0S1.pdf");
+  c3->SaveAs("I1S0.pdf");
+  c4->SaveAs("I1S1.pdf");
   outfile->cd();
   ListI0S0->Write("I0S0", 1);
   ListI0S1->Write("I0S1", 1);
