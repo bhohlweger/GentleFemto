e5230f6e611d4efc5fb932a81978077067503ae2
Andreas Mathis <andreas.mathis@cern.ch>
2019-01-18 15:08:38 +0100

    Add reweighting QA and deal with less histograms when using the syst. vars.

--- DreamFunction/Scripts/ExecuteSigma.C
+++ DreamFunction/Scripts/ExecuteSigma.C
@@ -1,14 +1,15 @@
 #include "GetSigmaCorrelations.C"
-#include "METoSEReweighting.C"
+#include "METoSEReweightingSigma0.C"
 
 int main(int argc, char* argv[]) {
   const char* filename = argv[1];
-  const char* suffix = argv[2];
-  GetSigmaCorrelations(filename, suffix);
+  const char* trigger = argv[2];
+  const char* suffix = argv[3];
+  GetSigmaCorrelations(filename, trigger, suffix);
 
   TString foldername = filename;
   foldername.ReplaceAll("AnalysisResults.root", "");
-//  METoSEReweighting(foldername.Data());
+  METoSEReweightingSigma0(foldername.Data());
 
   return 1;
 }
