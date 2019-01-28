7fb16d811777db5b700f8c1b0ec284d0551144f7
Andreas Mathis <andreas.mathis@cern.ch>
2019-01-28 14:06:11 +0100

    Add reweighting debug output for the Sigma

---
+++ DreamFunction/Scripts/METoSEReweightingSigma0.C
@@ -0,0 +1,21 @@
+#include "METoSEReweighting.C"
+
+void METoSEReweightingSigma0(const char* foldername) {
+  const char* filenames[6] = { "pp", "pSigma", "pSB_low", "pSB_up", "pLambda",
+      "pPhoton" };
+  for (int iFile = 0; iFile < 6; ++iFile) {
+    TString FileName = Form("%sCFOutput_%s.root", foldername, filenames[iFile]);
+    std::cout << FileName.Data() << std::endl;
+    TFile* file = TFile::Open(FileName, "update");
+    TList* PairDist = (TList*) file->Get("PairDist");
+    if (PairDist) {
+      ReweightingQA(PairDist);
+    } else {
+      file->ls();
+    }
+    TList* AntiPairDist = (TList*) file->Get("AntiPairDist");
+    if (AntiPairDist) {
+      ReweightingQA(AntiPairDist);
+    }
+  }
+}
