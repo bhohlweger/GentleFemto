a5284378057a2e9d6e1998fa6b1009b2b5c9da1a
Andreas Mathis <andreas.mathis@cern.ch>
2019-02-05 17:55:35 +0100

    Enable imaginary scattering lengths for Sigma0 Lednicky

--- GentleKitty/Scripts/DrawSigma_single.C
+++ GentleKitty/Scripts/DrawSigma_single.C
@@ -4,6 +4,6 @@
 // main
 int main(int argc, char *argv[]) {
   DrawSigma(atoi(argv[1]), argv[2], argv[3], argv[4], atoi(argv[5]),
-	    atof(argv[6]), atof(argv[7]));
+            atof(argv[6]), atof(argv[7]), atof(argv[8]));
   return 0;
 }
