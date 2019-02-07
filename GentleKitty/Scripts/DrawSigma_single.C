515005ef861635f6e384798e34e114ef53591dab
Andreas Mathis <andreas.mathis@cern.ch>
2019-02-07 13:04:08 +0100

    Remove unnecessary executable

--- GentleKitty/Scripts/DrawSigma_single.C
+++
@@ -1,9 +0,0 @@
-#include "DrawSigma.C"
-
-// =========================================
-// main
-int main(int argc, char *argv[]) {
-  DrawSigma(atoi(argv[1]), argv[2], argv[3], argv[4], atoi(argv[5]),
-            atof(argv[6]), atof(argv[7]), atof(argv[8]));
-  return 0;
-}
--- GentleKitty/Scripts/pSigma0_single.C
+++ GentleKitty/Scripts/pSigma0_single.C
@@ -1,8 +1,11 @@
 #include "pSigma0.C"
+#include "DrawSigma.C"
 
 /// =====================================================================================
 int main(int argc, char *argv[]) {
   FitSigma0(atoi(argv[1]), argv[2], argv[3], argv[4], atoi(argv[5]),
+            atof(argv[6]), atof(argv[7]), atof(argv[8]));
+  DrawSigma(atoi(argv[1]), argv[2], argv[3], argv[4], atoi(argv[5]),
             atof(argv[6]), atof(argv[7]), atof(argv[8]));
   return 0;
 }
