2743d1561adbe662348e4a75e38acfc13ea05552
bhohlweger <bernhard.hohlweger@cern.ch>
2019-02-27 17:00:39 +0100

    Splits the pp and pXi systematic in two executables

--- Systematics/EvalDreamSyspp.C
+++ Systematics/EvalDreamSyspp.C
@@ -1,41 +1,43 @@
 #include "CATSInput.h"
 #include "DreamPlot.h"
 #include "DreamSystematics.h"
+#include "ForgivingReader.h"
+#include "CandidateCounter.h"
 #include "TCanvas.h"
 #include <iostream>
 
 void EvalDreamSystematics(TString InputDir, TString prefix) {
   TString filename = Form("%s/AnalysisResults.root", InputDir.Data());
-  const int rebin = 5;
-
   DreamPlot::SetStyle();
   auto CATSinput = new CATSInput();
-  CATSinput->SetNormalization(0.240,0.340);
+  CATSinput->SetNormalization(0.240, 0.340);
+
+  auto counter = new CandidateCounter();
+
   ReadDreamFile* DreamFile = new ReadDreamFile(6, 6);
   DreamFile->SetAnalysisFile(filename.Data(), prefix);
+
+  //Proton - Proton
   DreamDist* pp = DreamFile->GetPairDistributions(0, 0, "");
   DreamDist* ApAp = DreamFile->GetPairDistributions(1, 1, "");
-  DreamCF* CFDef = CATSinput->ObtainCFSyst(10, "ppDef", pp, ApAp);
-  auto dataHistProton = CATSinput->FindHistogram(
-      CFDef->GetCorrelationFunctions(), "hCk_RebinnedppDefMeV_0");
+  DreamCF* CFppDef = CATSinput->ObtainCFSyst(10, "ppDef", pp, ApAp);
   DreamSystematics protonproton(DreamSystematics::pp);
-  protonproton.SetDefaultHist(dataHistProton);
+  protonproton.SetDefaultPair(CFppDef, "hCk_RebinnedppDefMeV_0");
+
   const int protonVarStart = 1;
   for (int i = protonVarStart;
       i < protonVarStart + protonproton.GetNumberOfVars(); ++i) {
     ReadDreamFile* DreamVarFile = new ReadDreamFile(6, 6);
     DreamVarFile->SetAnalysisFile(filename.Data(), prefix, Form("%u", i));
     TString VarName = TString::Format("ppVar%u", i);
-    DreamCF* CFVar = CATSinput->ObtainCFSyst(
+    DreamCF* CFppVar = CATSinput->ObtainCFSyst(
         10, VarName.Data(), DreamVarFile->GetPairDistributions(0, 0, ""),
         DreamVarFile->GetPairDistributions(1, 1, ""), pp, ApAp);
-    protonproton.SetVarHist(
-        CATSinput->FindHistogram(CFVar->GetCorrelationFunctions(),
-                                 TString::Format("Rebinned%sMeV", VarName.Data())));
-//    delete DreamVarFile;
-//    delete CFVar;
+    protonproton.SetVarPair(CFppVar,
+                            TString::Format("Rebinned%sMeV", VarName.Data()));
   }
   protonproton.EvalSystematics();
+  protonproton.EvalDifferenceInPairs();
   protonproton.WriteOutput();
   protonproton.DrawAllCF();
 }
