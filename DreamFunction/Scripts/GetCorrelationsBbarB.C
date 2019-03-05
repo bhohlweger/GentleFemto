c289ed3772453baa3e9b3c8b5884dc36693a229e
Valentina Mantovani Sarti <valentina.mantovani-sarti@tum.de>
2019-03-05 12:34:32 +0100

    Removes non existing cfs and adds spericity output

--- DreamFunction/Scripts/GetCorrelationsBbarB.C
+++ DreamFunction/Scripts/GetCorrelationsBbarB.C
@@ -5,26 +5,36 @@
 #include <iostream>
 
 void GetCorrelationsBbarB(const char* filename, const char* prefix,
-                     const char* addon = "") {
+                     const char* addon = "", const bool isMC = false,
+                     const bool epos = false) {
   //gStyle->SetOptStat(0);
-  ReadDreamFile* DreamFile = new ReadDreamFile(6, 6);
+  ReadDreamFile* DreamFile = new ReadDreamFile(4, 4);
   DreamFile->SetAnalysisFile(filename, prefix, addon);
 
+  TString add1="1";
+  TString add2="2";
+  TString add3="3";
+  TString add4="4";
+  TString add5="5";
+
+  Double_t norm1=0.2;
+  Double_t norm2=0.4;
+
   DreamCF* CF_pAp_App = new DreamCF();
-  DreamPair* pAp = new DreamPair("PartAntiPart", 0.24, 0.34);
+  DreamPair* pAp = new DreamPair("PartAntiPart", norm1, norm2);
 //  DreamPair* App = new DreamPair("AntiPartPart", 0.2, 0.4);
 
   DreamCF* CF_pAL_ApL = new DreamCF();
-  DreamPair* pAL = new DreamPair("PartAntiPart", 0.24, 0.34);
-  DreamPair* ApL = new DreamPair("AntiPartPart", 0.24, 0.34);
+  DreamPair* pAL = new DreamPair("PartAntiPart", norm1, norm2);
+  DreamPair* ApL = new DreamPair("AntiPartPart", norm1, norm2);
 
   DreamCF* CF_ALL_LAL = new DreamCF();
-  DreamPair* LAL = new DreamPair("PartAntiPart", 0.24, 0.34);
+  DreamPair* LAL = new DreamPair("PartAntiPart", norm1, norm2);
 //  DreamPair* ALL = new DreamPair("AntiPartPart", 0.2, 0.4);
 
   DreamCF* CF_pAXi_ApXi = new DreamCF();
-  DreamPair* pAXi = new DreamPair("PartAntiPart", 0.24, 0.34);
-  DreamPair* ApXi = new DreamPair("AntiPartPart", 0.24, 0.34);
+  DreamPair* pAXi = new DreamPair("PartAntiPart", norm1, norm2);
+  DreamPair* ApXi = new DreamPair("AntiPartPart", norm1, norm2);
 
   std::cout << "=========================" << std::endl;
   std::cout << "========Pair Set=========" << std::endl;
@@ -38,8 +48,8 @@ void GetCorrelationsBbarB(const char* filename, const char* prefix,
   LAL->SetPair(DreamFile->GetPairDistributions(2, 3, ""));
 //  ALL->SetPair(DreamFile->GetPairDistributions(3, 2, ""));
 
-  pAXi->SetPair(DreamFile->GetPairDistributions(0, 5, ""));
-  ApXi->SetPair(DreamFile->GetPairDistributions(1, 4, ""));
+  pAXi->SetPair(DreamFile->GetPairDistributions(1, 2, ""));
+  ApXi->SetPair(DreamFile->GetPairDistributions(2, 2, ""));
 
   std::cout << "=========================" << std::endl;
   std::cout << "======Pair Shifted=======" << std::endl;
@@ -47,10 +57,10 @@ void GetCorrelationsBbarB(const char* filename, const char* prefix,
   pAp->ShiftForEmpty(pAp->GetPair());
 //  App->ShiftForEmpty(App->GetPair());
 
-  pAL->ShiftForEmpty(pAL->GetPair());
-  ApL->ShiftForEmpty(ApL->GetPair());
+ pAL->ShiftForEmpty(pAL->GetPair());
+ ApL->ShiftForEmpty(ApL->GetPair());
 
-  LAL->ShiftForEmpty(LAL->GetPair());
+ LAL->ShiftForEmpty(LAL->GetPair());
 //  ALL->ShiftForEmpty(ALL->GetPair());
 
   pAXi->ShiftForEmpty(pAXi->GetPair());
@@ -59,7 +69,7 @@ void GetCorrelationsBbarB(const char* filename, const char* prefix,
   std::cout << "=========================" << std::endl;
   std::cout << "====Pair Fix Shifted=====" << std::endl;
   std::cout << "=========================" << std::endl;
-  //Fix shift singe pair cfs anyway to ensure competability!
+  //Fix shift singe pair cfs anyway to ensure compatibility!
   pAp->FixShift(pAp->GetPairShiftedEmpty(0), pAp->GetPairShiftedEmpty(0),
                 pAp->GetFirstBin());
   pAL->FixShift(pAL->GetPairShiftedEmpty(0), ApL->GetPairShiftedEmpty(0),
@@ -68,7 +78,6 @@ void GetCorrelationsBbarB(const char* filename, const char* prefix,
                  pAL->GetFirstBin());
   LAL->FixShift(LAL->GetPairShiftedEmpty(0), LAL->GetPairShiftedEmpty(0),
                 LAL->GetFirstBin());
-
   pAXi->FixShift(pAXi->GetPairShiftedEmpty(0), ApXi->GetPairShiftedEmpty(0),
                 ApXi->GetFirstBin());
   ApXi->FixShift(ApXi->GetPairShiftedEmpty(0), pAXi->GetPairShiftedEmpty(0),
@@ -111,29 +120,151 @@ void GetCorrelationsBbarB(const char* filename, const char* prefix,
   std::cout << "=========CFs=============" << std::endl;
   std::cout << "=========================" << std::endl;
 
-  TString foldername = filename;
-  foldername.ReplaceAll("AnalysisResults.root", "");
-  std::cout << "$PWD " << foldername.Data() << std::endl;
+if(!isMC)
+{
+  TString foldername = "/Users/Valentina/cernbox/Analysis/BBbar/GentleFemto_Output/data/";
+
+  std::cout << "$PWD " << foldername << std::endl;
   std::cout << "pp CF \n";
   std::cout << "Set Pair \n";
   CF_pAp_App->SetPairs(pAp, nullptr);
   std::cout << "Get CF \n";
   CF_pAp_App->GetCorrelations();
   std::cout << "Write Output \n";
-  CF_pAp_App->WriteOutput(Form("%sCFOutput_pAp_App.root", foldername.Data()));
-
+  if(strcmp(addon, add1)==0){
+  std::cout << "Sphericity [0.,0.3] \n";
+  CF_pAp_App->WriteOutput(Form("%sCFOutput_pAp_App_st1.root", foldername.Data()));
+}else if(strcmp(addon, add2)==0){
+  std::cout << "Sphericity [0.3,0.7] \n";
+  CF_pAp_App->WriteOutput(Form("%sCFOutput_pAp_App_st2.root", foldername.Data()));
+}else if(strcmp(addon, add3)==0){
+  std::cout << "Sphericity [0.7,1.] \n";
+  CF_pAp_App->WriteOutput(Form("%sCFOutput_pAp_App_st3.root", foldername.Data()));
+}else if(strcmp(addon, add4)==0){
+  std::cout << "No Sphericity Cuts \n";
+  CF_pAp_App->WriteOutput(Form("%sCFOutput_pAp_App_full.root", foldername.Data()));
+  }
+  else if(strcmp(addon, add5)==0){
+    std::cout << "Sphericity Cuts [0.9-1]\n";
+    CF_pAp_App->WriteOutput(Form("%sCFOutput_pAp_App_st5.root", foldername.Data()));
+    }
+;
   std::cout << "pL CF \n";
   CF_pAL_ApL->SetPairs(pAL, ApL);
   CF_pAL_ApL->GetCorrelations();
-  CF_pAL_ApL->WriteOutput(Form("%sCFOutput_pAL_ApL.root", foldername.Data()));
+  if(strcmp(addon, add1)==0){
+  std::cout << "Sphericity [0.,0.3] \n";
+  CF_pAL_ApL->WriteOutput(Form("%sCFOutput_pAL_ApL_st1.root", foldername.Data()));
+}else if(strcmp(addon, add2)==0){
+  std::cout << "Sphericity [0.3,0.7] \n";
+  CF_pAL_ApL->WriteOutput(Form("%sCFOutput_pAL_ApL_st2.root", foldername.Data()));
+}else if(strcmp(addon, add3)==0){
+  std::cout << "Sphericity [0.7,1.] \n";
+  CF_pAL_ApL->WriteOutput(Form("%sCFOutput_pAL_ApL_st3.root", foldername.Data()));
+}else if(strcmp(addon, add4)==0){
+  std::cout << "No Sphericity Cuts \n";
+  CF_pAL_ApL->WriteOutput(Form("%sCFOutput_pAL_ApL_full.root", foldername.Data()));
+}else if(strcmp(addon, add5)==0){
+  std::cout << "Sphericity Cuts [0.9-1]\n";
+  CF_pAL_ApL->WriteOutput(Form("%sCFOutput_pAL_ApL_st5.root", foldername.Data()));
+}
+;
 
   std::cout << "LL CF \n";
   CF_ALL_LAL->SetPairs(LAL, nullptr);
   CF_ALL_LAL->GetCorrelations();
-  CF_ALL_LAL->WriteOutput(Form("%sCFOutput_LAL_ALL.root", foldername.Data()));
+  if(strcmp(addon, add1)==0){
+  std::cout << "Sphericity [0.,0.3] \n";
+  CF_ALL_LAL->WriteOutput(Form("%sCFOutput_LAL_ALL_st1.root", foldername.Data()));
+}else if(strcmp(addon, add2)==0){
+  std::cout << "Sphericity [0.3,0.7] \n";
+  CF_ALL_LAL->WriteOutput(Form("%sCFOutput_LAL_ALL_st2.root", foldername.Data()));
+}else if(strcmp(addon, add3)==0){
+  std::cout << "Sphericity [0.7,1.] \n";
+  CF_ALL_LAL->WriteOutput(Form("%sCFOutput_LAL_ALL_st3.root", foldername.Data()));
+}else if(strcmp(addon, add4)==0){
+  std::cout << "No Sphericity Cuts \n";
+  CF_ALL_LAL->WriteOutput(Form("%sCFOutput_LAL_ALL_full.root", foldername.Data()));
+}else if(strcmp(addon, add5)==0){
+  std::cout << "Sphericity Cuts [0.9-1]\n";
+  CF_ALL_LAL->WriteOutput(Form("%sCFOutput_LAL_ALL_st5.root", foldername.Data()));
+}
+;
+} else if (isMC)
+{
+  TString foldername;
+  if(!epos)
+  {
+  foldername = "/Users/Valentina/cernbox/Analysis/BBbar/GentleFemto_Output/mc/Pythia/";
+  }else if(epos){
+  foldername = "/Users/Valentina/cernbox/Analysis/BBbar/GentleFemto_Output/mc/epos/";
+  }
+  std::cout << "$PWD " << foldername << std::endl;
+  std::cout << "pp CF \n";
+  std::cout << "Set Pair \n";
+  CF_pAp_App->SetPairs(pAp, nullptr);
+  std::cout << "Get CF \n";
+  CF_pAp_App->GetCorrelations();
+  std::cout << "Write Output \n";
+  if(strcmp(addon, add1)==0){
+  std::cout << "Sphericity [0.,0.3] \n";
+  CF_pAp_App->WriteOutput(Form("%sMCCFOutput_pAp_App_st1.root", foldername.Data()));
+}else if(strcmp(addon, add2)==0){
+  std::cout << "Sphericity [0.3,0.7] \n";
+  CF_pAp_App->WriteOutput(Form("%sMCCFOutput_pAp_App_st2.root", foldername.Data()));
+}else if(strcmp(addon, add3)==0){
+  std::cout << "Sphericity [0.7,1.] \n";
+  CF_pAp_App->WriteOutput(Form("%sMCCFOutput_pAp_App_st3.root", foldername.Data()));
+}else if(strcmp(addon, add4)==0){
+  std::cout << "No Sphericity Cuts \n";
+  CF_pAp_App->WriteOutput(Form("%sMCCFOutput_pAp_App_full.root", foldername.Data()));
+  }
+  else if(strcmp(addon, add5)==0){
+    std::cout << "Sphericity Cuts [0.9-1]\n";
+    CF_pAp_App->WriteOutput(Form("%sMCCFOutput_pAp_App_st5.root", foldername.Data()));
+    }
+;
+  std::cout << "pL CF \n";
+  CF_pAL_ApL->SetPairs(pAL, ApL);
+  CF_pAL_ApL->GetCorrelations();
+  if(strcmp(addon, add1)==0){
+  std::cout << "Sphericity [0.,0.3] \n";
+  CF_pAL_ApL->WriteOutput(Form("%sMCCFOutput_pAL_ApL_st1.root", foldername.Data()));
+}else if(strcmp(addon, add2)==0){
+  std::cout << "Sphericity [0.3,0.7] \n";
+  CF_pAL_ApL->WriteOutput(Form("%sMCCFOutput_pAL_ApL_st2.root", foldername.Data()));
+}else if(strcmp(addon, add3)==0){
+  std::cout << "Sphericity [0.7,1.] \n";
+  CF_pAL_ApL->WriteOutput(Form("%sMCCFOutput_pAL_ApL_st3.root", foldername.Data()));
+}else if(strcmp(addon, add4)==0){
+  std::cout << "No Sphericity Cuts \n";
+  CF_pAL_ApL->WriteOutput(Form("%sMCCFOutput_pAL_ApL_full.root", foldername.Data()));
+}else if(strcmp(addon, add5)==0){
+  std::cout << "Sphericity Cuts [0.9-1]\n";
+  CF_pAL_ApL->WriteOutput(Form("%sMCCFOutput_pAL_ApL_st5.root", foldername.Data()));
+}
+;
 
-  std::cout << "pXi CF \n";
-  CF_pAXi_ApXi->SetPairs(pAXi, ApXi);
-  CF_pAXi_ApXi->GetCorrelations();
-  CF_pAXi_ApXi->WriteOutput(Form("%sCFOutput_pAXi_ApXi.root", foldername.Data()));
+  std::cout << "LL CF \n";
+  CF_ALL_LAL->SetPairs(LAL, nullptr);
+  CF_ALL_LAL->GetCorrelations();
+  if(strcmp(addon, add1)==0){
+  std::cout << "Sphericity [0.,0.3] \n";
+  CF_ALL_LAL->WriteOutput(Form("%sMCCFOutput_LAL_ALL_st1.root", foldername.Data()));
+}else if(strcmp(addon, add2)==0){
+  std::cout << "Sphericity [0.3,0.7] \n";
+  CF_ALL_LAL->WriteOutput(Form("%sMCCFOutput_LAL_ALL_st2.root", foldername.Data()));
+}else if(strcmp(addon, add3)==0){
+  std::cout << "Sphericity [0.7,1.] \n";
+  CF_ALL_LAL->WriteOutput(Form("%sMCCFOutput_LAL_ALL_st3.root", foldername.Data()));
+}else if(strcmp(addon, add4)==0){
+  std::cout << "No Sphericity Cuts \n";
+  CF_ALL_LAL->WriteOutput(Form("%sMCCFOutput_LAL_ALL_full.root", foldername.Data()));
+}else if(strcmp(addon, add5)==0){
+  std::cout << "Sphericity Cuts [0.9-1]\n";
+  CF_ALL_LAL->WriteOutput(Form("%sMCCFOutput_LAL_ALL_st5.root", foldername.Data()));
+}
+};
+
+
 }
