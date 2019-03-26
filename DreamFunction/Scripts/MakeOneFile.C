766ff3596e810e0501cef59f973e479b20d1e6de
bhohlweg <bhohlweg@cern.ch>
2019-03-26 14:35:22 +0100

    Adds a new script to add files

---
+++ DreamFunction/Scripts/MakeOneFile.C
@@ -0,0 +1,30 @@
+#include "TFile.h"
+#include "TGraph.h"
+#include "TROOT.h"
+#include "TSystem.h"
+#include "TKey.h"
+#include <iostream>
+void MakeOneFile(const char* outputFile, const char* outputName,
+                 const char* inputFile) {
+  TFile* outfile = TFile::Open(outputFile, "UPDATE");
+  TFile* infile = TFile::Open(inputFile, "READ");
+  TIter next(infile->GetListOfKeys());
+  TKey* key;
+  while ((key = (TKey*) next())) {
+    TClass *cl = gROOT->GetClass(key->GetClassName());
+    if (!cl->InheritsFrom("TGraph"))
+      continue;
+    TGraph *Graph = (TGraph*) key->ReadObj();
+    std::cout << Graph->GetName() << std::endl;
+    outfile->cd();
+    Graph->Write(TString::Format("%s%s", outputName, Graph->GetName()));
+  }
+  infile->Close();
+  outfile->Close();
+  return;
+}
+
+int main(int argc, char* argv[]) {
+  MakeOneFile(argv[1], argv[2], argv[3]);
+  return 0;
+}
