89e80150cd803a6aaa27ff995138593a30a48cd4
Valentina Mantovani Sarti <valentina.mantovani-sarti@tum.de>
2019-03-27 11:39:35 +0100

    Adding executable for CF BBar, Scripts for quick plots for AN

---
+++ DreamFunction/Scripts/ExecuteQuickPlotsPL.C
@@ -0,0 +1,14 @@
+#include "GetQuickPlotsPL.C"
+#include "BbarB_QAplots.C"
+#include "BbarB_PurityPlots.C"
+#include "GetCorrelationsBbarB.C"
+#include "GetComparisonMC.C"
+#include "METoSEReweighting.C"
+
+int main(int argc, char* argv[]) {
+
+  GetQuickPlotsPL();
+
+
+  return 1;
+}
