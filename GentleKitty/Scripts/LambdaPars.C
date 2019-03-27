091df2a42a27cc6640e47b15d87502bf99fd5190
bhohlweger <bernhard.hohlweger@cern.ch>
2019-03-27 16:02:10 +0100

    Adds the correct fractions for pp and pXi correlations in pp HM

--- GentleKitty/Scripts/LambdaPars.C
+++ GentleKitty/Scripts/LambdaPars.C
@@ -1,14 +1,15 @@
 #include "CATSLambdaParam.h"
 
 void LambdaPP() {
-  double PurityProton = 1.;
-  double protonPrim = 0.9;
-  double protonSecLam = 0.1;
+  double PurityProton = 0.9943;
+  double protonPrim = 0.873;
+  double protonSecLam = 0.089;
+  double protonSecSig = 0.037;
 
   const Particle p1(
       PurityProton,
       protonPrim,
-      { protonSecLam});
+      { protonSecLam, protonSecSig});
   const Particle p2 = p1;
   //
   CATSLambdaParam params(p1, p2, true);
@@ -22,7 +23,7 @@ void LambdaPXi() {
   double protonSecLam = 0.089;
   double protonSecSig = 0.037;
 
-  double PurityXi = 0.88;
+  double PurityXi = 0.915;
   const double Xim1530_to_Xim = 0.32 * (1. / 3.);
   const double Xin1530_to_Xim = 0.32 * (2. / 3.);
   const double Omegam_to_Xim = 0.1;
@@ -44,6 +45,6 @@ void LambdaPXi() {
 
 int main(int argc, char* argv[]) {
   LambdaPP();
-//  LambdaPXi();
+  LambdaPXi();
   return 0;
 }
