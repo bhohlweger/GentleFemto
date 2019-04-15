bb260361f80fa48eb705c691fce4d75d2176e101
Andreas Mathis <andreas.mathis@cern.ch>
2019-04-15 18:02:16 +0200

    Make root shut up

--- GentleKitty/Scripts/pSigma0_single.C
+++ GentleKitty/Scripts/pSigma0_single.C
@@ -1,8 +1,10 @@
 #include "pSigma0.C"
 #include "DrawSigma.C"
+#include "TROOT.h"
 
 /// =====================================================================================
 int main(int argc, char *argv[]) {
+  gROOT->ProcessLine("gErrorIgnoreLevel = 3001");
   FitSigma0(argv);
   DrawSigma(argv);
   return 0;
