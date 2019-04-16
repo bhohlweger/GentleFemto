c8c1a283acfe9c2c5aef8373dd6148ebc445648c
bhohlweger <bernhard.hohlweger@cern.ch>
2019-04-16 16:51:45 +0200

    Adds the inFile for the mT bins

--- GentleKitty/Scripts/ppAnalysis/pp_mTSourceExtractor.C
+++ GentleKitty/Scripts/ppAnalysis/pp_mTSourceExtractor.C
@@ -1,11 +1,13 @@
 #include "TFile.h"
+#include "TGraphErrors.h"
 
-void mTRadiusExtractor(const char *inputFile) {
-  TFile* inFile = TFile::Open(inputFile);
-
+void mTRadiusExtractor(const char *inputFile, const char* mTFile) {
+  TFile* inFile = TFile::Open(inputFile,"READ");
+  TFile* inmTFile = TFile::Open(mTFile,"READ");
+  TGraphErrors* AveragemT = (TGraphErrors*)inmTFile->Get("AveragemT");
 }
 
 int main(int argc, char *argv[]) {
-  mTRadiusExtractor(argv[1]);
+  mTRadiusExtractor(argv[1],argv[2]);
   return 0;
 }
