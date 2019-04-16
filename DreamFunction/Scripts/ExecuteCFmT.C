80628f2dda598184b0d48dfb3a4117ca44e50912
bhohlweger <bernhard.hohlweger@cern.ch>
2019-04-16 16:52:47 +0200

    Changes the binning to a more accurate version for p-p

--- DreamFunction/Scripts/ExecuteCFmT.C
+++ DreamFunction/Scripts/ExecuteCFmT.C
@@ -17,8 +17,7 @@ int main(int argc, char* argv[]) {
   DreamFile->ReadmTHistos(filename, prefix, addon);
   mTppDists = DreamFile->GetmTPairDistributions(0, 0, 1, 1);
 
-  std::vector<float> mTppBins = { 0.9, 1.07, 1.13, 1.19, 1.25, 1.37, 1.55, 1.97,
-      4.5 };
+  std::vector<float> mTppBins = { 1.02, 1.14, 1.2, 1.26, 1.38, 1.56, 1.86, 4.5};
 //  std::vector<float> mTBins = { 1.01,1.13,1.25,1.55,4.5 };
   TString CalibPP = Form("%s/CFOutput_pp.root", CalibPath);
   mTppDists->SetSEMEReweightingRatio(CalibPP, "1", false);
@@ -27,32 +26,32 @@ int main(int argc, char* argv[]) {
   mTppDists->SetRebin( { 2 });
   mTppDists->ObtainTheCorrelationFunction(gSystem->pwd(), prefix, "pp");
 
-  DreamKayTee* mTpLDists;
-  DreamFile->ReadmTHistos(filename, prefix, addon);
-  mTpLDists = DreamFile->GetmTPairDistributions(0, 2, 1, 3);
-
-  std::vector<float> mTpLBins = { 1.02, 1.26, 1.32, 1.44, 1.62, 1.68, 4.5 };
-
-  TString CalibPL = Form("%s/CFOutput_pL.root",CalibPath);
-  mTpLDists->SetSEMEReweightingRatio(CalibPL, "1", true);
-  mTpLDists->SetKayTeeBins(mTpLBins);
-  mTpLDists->SetNormalization(0.2, 0.4);
-  mTpLDists->ObtainTheCorrelationFunction(gSystem->pwd(), prefix, "pL");
-
-  DreamKayTee* mTpXiDists;
-  DreamFile->ReadmTHistos(filename, prefix, addon);
-  mTpXiDists = DreamFile->GetmTPairDistributions(0, 4, 1, 5);
-
-//  std::vector<float> mTpXiBins = { 1.08, 4.5 };
-  std::vector<float> mTpXiBins = { 1.08, 1.83, 4.5 };
-
-  TString CalibpXi = Form("%s/CFOutput_pXi.root",CalibPath);
-  mTpXiDists->SetSEMEReweightingRatio(CalibpXi, "1", true);
-  mTpXiDists->SetKayTeeBins(mTpXiBins);
-  mTpXiDists->SetNormalization(0.2, 0.4);
-  mTpXiDists->SetRebin({5});
-  mTpXiDists->FixShift({true,true},{0.012,0.008});
-  mTpXiDists->ObtainTheCorrelationFunction(gSystem->pwd(), prefix, "pXi");
+//  DreamKayTee* mTpLDists;
+//  DreamFile->ReadmTHistos(filename, prefix, addon);
+//  mTpLDists = DreamFile->GetmTPairDistributions(0, 2, 1, 3);
+//
+//  std::vector<float> mTpLBins = { 1.02, 1.26, 1.32, 1.44, 1.62, 1.68, 4.5 };
+//
+//  TString CalibPL = Form("%s/CFOutput_pL.root",CalibPath);
+//  mTpLDists->SetSEMEReweightingRatio(CalibPL, "1", true);
+//  mTpLDists->SetKayTeeBins(mTpLBins);
+//  mTpLDists->SetNormalization(0.2, 0.4);
+//  mTpLDists->ObtainTheCorrelationFunction(gSystem->pwd(), prefix, "pL");
+//
+//  DreamKayTee* mTpXiDists;
+//  DreamFile->ReadmTHistos(filename, prefix, addon);
+//  mTpXiDists = DreamFile->GetmTPairDistributions(0, 4, 1, 5);
+//
+////  std::vector<float> mTpXiBins = { 1.08, 4.5 };
+//  std::vector<float> mTpXiBins = { 1.08, 1.83, 4.5 };
+//
+//  TString CalibpXi = Form("%s/CFOutput_pXi.root",CalibPath);
+//  mTpXiDists->SetSEMEReweightingRatio(CalibpXi, "1", true);
+//  mTpXiDists->SetKayTeeBins(mTpXiBins);
+//  mTpXiDists->SetNormalization(0.2, 0.4);
+//  mTpXiDists->SetRebin({5});
+//  mTpXiDists->FixShift({true,true},{0.012,0.008});
+//  mTpXiDists->ObtainTheCorrelationFunction(gSystem->pwd(), prefix, "pXi");
 
   return 1;
 }
