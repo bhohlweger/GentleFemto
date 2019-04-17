7a53defca22b91e82c3d960caf4f850aa92e9890
bhohlweg <bhohlweg@cern.ch>
2019-04-17 08:15:21 +0200

    Adds a script to extract the mT vs. radius scaling.

--- GentleKitty/Scripts/ppAnalysis/pp_mTSourceExtractor.C
+++ GentleKitty/Scripts/ppAnalysis/pp_mTSourceExtractor.C
@@ -1,13 +1,47 @@
 #include "TFile.h"
 #include "TGraphErrors.h"
+#include "TNtuple.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+#include <iostream>
+void mTRadiusExtractor(const char *inputFile, float &radius, float &radErr) {
+  TFile* inFile = TFile::Open(inputFile, "READ");
+  if(!inFile) {
+    std::cout << "No input found for " << inputFile << ", exiting \n";
+    return;
+  }
+  TNtuple* mTSysVar = (TNtuple*) inFile->Get("ntResult");
+  auto histRad = new TH1D("hRad", "hRad", 200, 0.4, 1.2);
+  mTSysVar->Draw("Radius_pp>>hRad");
+  radius = histRad->GetMean();
+  radErr = histRad->GetRMS();
+  delete histRad;
+}
 
-void mTRadiusExtractor(const char *inputFile, const char* mTFile) {
-  TFile* inFile = TFile::Open(inputFile,"READ");
-  TFile* inmTFile = TFile::Open(mTFile,"READ");
-  TGraphErrors* AveragemT = (TGraphErrors*)inmTFile->Get("AveragemT");
+void mTRadiusPlot(const char* inputFolder, const char* avgmTFile) {
+  TFile* mTFile = TFile::Open(avgmTFile, "READ");
+  TGraphErrors* avgmT = (TGraphErrors*) mTFile->Get("AveragemT");
+  TGraphErrors* mTRadius = new TGraphErrors();
+  for (int imT = 0; imT < 6; ++imT) {
+    double mT, dummy;
+    float radius, radErr;
+    TString inputFile = TString::Format("%s/OutFileVarpp_%u.root",
+                                        inputFolder, imT);
+    avgmT->GetPoint(imT, dummy, mT);
+    std::cout << avgmT->GetErrorY(imT) << std::endl;
+    mTRadiusExtractor(inputFile.Data(), radius, radErr);
+    std::cout << "radius: " << radius << " radErr: " << radErr << std::endl;
+    mTRadius->SetPoint(imT, mT, radius);
+    mTRadius->SetPointError(imT, avgmT->GetErrorY(imT), radErr);
+  }
+  TFile* output = TFile::Open("mTRad.root", "RECREATE");
+  output->cd();
+  mTRadius->SetName("mTRadius");
+  mTRadius->Write();
+  output->Close();
 }
 
 int main(int argc, char *argv[]) {
-  mTRadiusExtractor(argv[1],argv[2]);
+  mTRadiusPlot(argv[1], argv[2]);
   return 0;
 }
