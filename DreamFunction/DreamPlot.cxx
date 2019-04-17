38c91b4773df6e0d84a0e49c6ad87d1d66d9e6b3
bhohlweger <bernhard.hohlweger@cern.ch>
2019-04-17 17:01:42 +0200

    Adjust the Plotting range to dimitars wishes

--- DreamFunction/DreamPlot.cxx
+++ DreamFunction/DreamPlot.cxx
@@ -412,7 +412,7 @@ void DreamPlot::DrawCorrelationFunctions() {
   fProtonProton->SetLegendName("p-p #oplus #bar{p}-#bar{p}", "fpe");
   fProtonProton->SetLegendName("Coulomb + Argonne #nu_{18} (fit)", "l");
   fProtonProton->SetRangePlotting(0, 200, 0.8, 3.5);
-  fProtonProton->SetInletRangePlotting(50, 200, 0.94, 1.06);
+  fProtonProton->SetInletRangePlotting(50, 375, 0.94, 1.06);
   fProtonProton->SetInletCoordinates(0.27, 0.22, 0.95, 0.61);
   fProtonProton->SetNDivisions(505);
   fProtonProton->SetLegendCoordinates(
