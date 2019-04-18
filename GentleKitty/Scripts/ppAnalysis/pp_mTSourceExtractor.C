a956f9446b52ceb8fffc29393863008b994ca2ae
bhohlweger <bernhard.hohlweger@cern.ch>
2019-04-18 13:39:36 +0200

    Adds the statistical error evaulation of the radius

--- GentleKitty/Scripts/ppAnalysis/pp_mTSourceExtractor.C
+++ GentleKitty/Scripts/ppAnalysis/pp_mTSourceExtractor.C
@@ -4,7 +4,7 @@
 #include "TH1D.h"
 #include "TCanvas.h"
 #include <iostream>
-void mTRadiusExtractor(const char *inputFile, float &radius, float &radErr) {
+void mTRadiusExtractor(const char *inputFile, float &radius, float &radErrSys, float &radErrStat) {
   TFile* inFile = TFile::Open(inputFile, "READ");
   if(!inFile) {
     std::cout << "No input found for " << inputFile << ", exiting \n";
@@ -12,32 +12,40 @@ void mTRadiusExtractor(const char *inputFile, float &radius, float &radErr) {
   }
   TNtuple* mTSysVar = (TNtuple*) inFile->Get("ntResult");
   auto histRad = new TH1D("hRad", "hRad", 200, 0.4, 1.2);
+  auto histErrRad = new TH1D("hRadErr", "hRadErr", 50, 0., 0.1);
   mTSysVar->Draw("Radius_pp>>hRad");
+  mTSysVar->Draw("RadiusErr_pp>>hRadErr");
   radius = histRad->GetMean();
-  radErr = histRad->GetRMS();
+  radErrSys = histRad->GetRMS();
+  radErrStat = histErrRad->GetMean();
   delete histRad;
 }
 
 void mTRadiusPlot(const char* inputFolder, const char* avgmTFile) {
   TFile* mTFile = TFile::Open(avgmTFile, "READ");
   TGraphErrors* avgmT = (TGraphErrors*) mTFile->Get("AveragemT");
-  TGraphErrors* mTRadius = new TGraphErrors();
+  TGraphErrors* mTRadiusSyst = new TGraphErrors();
+  TGraphErrors* mTRadiusStat = new TGraphErrors();
   for (int imT = 0; imT < 7; ++imT) {
     double mT, dummy;
-    float radius, radErr;
+    float radius, radErrSyst, radErrStat;
     TString inputFile = TString::Format("%s/OutFileVarpp_%u.root",
                                         inputFolder, imT);
     avgmT->GetPoint(imT, dummy, mT);
     std::cout << avgmT->GetErrorY(imT) << std::endl;
-    mTRadiusExtractor(inputFile.Data(), radius, radErr);
-    std::cout << "radius: " << radius << " radErr: " << radErr << std::endl;
-    mTRadius->SetPoint(imT, mT, radius);
-    mTRadius->SetPointError(imT, avgmT->GetErrorY(imT), radErr);
+    mTRadiusExtractor(inputFile.Data(), radius, radErrSyst, radErrStat);
+    std::cout << "radius: " << radius << " radErrSyst: " << radErrSyst << " radErrStat: " << radErrStat << std::endl;
+    mTRadiusSyst->SetPoint(imT, mT, radius);
+    mTRadiusStat->SetPoint(imT, mT, radius);
+    mTRadiusSyst->SetPointError(imT, avgmT->GetErrorY(imT), radErrSyst);
+    mTRadiusStat->SetPointError(imT, avgmT->GetErrorY(imT), radErrStat);
   }
   TFile* output = TFile::Open("mTRad.root", "RECREATE");
   output->cd();
-  mTRadius->SetName("mTRadius");
-  mTRadius->Write();
+  mTRadiusSyst->SetName("mTRadiusSyst");
+  mTRadiusSyst->Write();
+  mTRadiusStat->SetName("mTRadiusStat");
+  mTRadiusStat->Write();
   output->Close();
 }
 
