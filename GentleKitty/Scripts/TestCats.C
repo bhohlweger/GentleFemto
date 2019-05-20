566a346f11c75c473a995c22b32f89c881b53612
bhohlweg <bhohlweg@cern.ch>
2019-05-20 09:28:47 +0200

    Plots the proton channels

--- GentleKitty/Scripts/TestCats.C
+++ GentleKitty/Scripts/TestCats.C
@@ -4,129 +4,110 @@
 #include "TH1F.h"
 #include "TLegend.h"
 #include "TCanvas.h"
+#include "TidyCats.h"
+#include "TDatabasePDG.h"
 
 void testCats() {
-  TFile* myfile =
-      TFile::Open(
-          "~/cernbox/HM13TeV/AnalysisData/ClosePairRej/SelectedPairs/fmTOutputProper.root",
-          "read");
-  TGraph* ppSource = (TGraph*) myfile->Get("hRad_pp");
-  ppSource->SetTitle("Source using a Gaussian core plus resonances");
-  ppSource->SetLineWidth(5);
-  ppSource->SetLineColor(kBlue);
-  ppSource->GetXaxis()->SetRangeUser(0., 8.);
-  ppSource->GetXaxis()->SetTitle("r (fm)");
-  ppSource->GetXaxis()->SetTitleSize(0.05);
-  ppSource->GetXaxis()->SetTitleOffset(0.95);
-  ppSource->GetXaxis()->SetLabelSize(0.05);
-//  ppSource->GetXaxis()->SetLabelOffset(0.95);
 
-  ppSource->GetXaxis()->CenterTitle(true);
-  ppSource->GetYaxis()->SetRangeUser(0., 0.55);
-  ppSource->GetYaxis()->SetTitle("S(r) (fm^{-1})");
-  ppSource->GetYaxis()->CenterTitle(true);
-  ppSource->GetYaxis()->SetTitleSize(0.05);
-  ppSource->GetYaxis()->SetTitleOffset(0.95);
-  ppSource->GetYaxis()->SetLabelSize(0.05);
-//  ppSource->GetYaxis()->SetLabelOffset(0.95);
+  double PotPars1S0[8] = { NN_AV18, v18_Coupled3P2, 1, 1, 1, 0, 0, 0 };
+  double PotPars1D2[8] = { NN_AV18, v18_Coupled3P2, 1, 1, 1, 0, 2, 2 };  //the last 3 digits are s,l,j
+  double PotPars3P0[8] = { NN_AV18, v18_Coupled3P2, 1, 1, 1, 1, 1, 0 };
+  double PotPars3P1[8] = { NN_AV18, v18_Coupled3P2, 1, 1, 1, 1, 1, 1 };
+  double PotPars3P2[8] = { NN_AV18, v18_Coupled3P2, 1, 1, 1, 1, 1, 2 };
+  double PotPars3P2Coupled[8] = { NN_AV18, v18_SingleChannelMagic, 1, 1, 1, 1, 1, 2 };  // magically accounts for the coupling to F something
 
-  TGraph* pLSource = (TGraph*) myfile->Get("hRad_pL");
-  pLSource->SetLineWidth(5);
-  pLSource->SetLineColor(30);
-  TGraph* pS0Source = (TGraph*) myfile->Get("hRad_pS0");
-  pS0Source->SetLineWidth(5);
-  pS0Source->SetLineColor(38);
-  TGraph* pXiSource = (TGraph*) myfile->Get("hRad_pXim");
-  pXiSource->SetLineWidth(5);
-  pXiSource->SetLineColor(46);
-  TGraph* pOmSource = (TGraph*) myfile->Get("hRad_pOmega");
-  pOmSource->SetLineWidth(5);
-  pOmSource->SetLineColor(93);
+  CATSparameters *cPotPars1S0 = new CATSparameters(CATSparameters::tPotential,
+                                                   8, true);
+  cPotPars1S0->SetParameters(PotPars1S0);
+  CATSparameters *cPotPars1D2 = new CATSparameters(CATSparameters::tPotential,
+                                                   8, true);
+  cPotPars1D2->SetParameters(PotPars1D2);
+  CATSparameters *cPotPars3P0 = new CATSparameters(CATSparameters::tPotential,
+                                                   8, true);
+  cPotPars3P0->SetParameters(PotPars3P0);
+  CATSparameters *cPotPars3P1 = new CATSparameters(CATSparameters::tPotential,
+                                                   8, true);
+  cPotPars3P1->SetParameters(PotPars3P1);
+  CATSparameters *cPotPars3P2 = new CATSparameters(CATSparameters::tPotential,
+                                                   8, true);
+  cPotPars3P2->SetParameters(PotPars3P2);
+  CATSparameters *cPotPars3P2Coupled = new CATSparameters(CATSparameters::tPotential,
+                                                   8, true);
+  cPotPars3P2Coupled->SetParameters(PotPars3P2Coupled);
+  const double massProton = TDatabasePDG::Instance()->GetParticle(2212)->Mass()
+      * 1000;
+  CATSparameters* cPars;
+  cPars = new CATSparameters(CATSparameters::tSource, 1, true);
+  cPars->SetParameter(0, 1.2);
 
-  TF1* ppGauss = (TF1*) myfile->Get("pp");
-  ppGauss->SetLineWidth(5);
-  ppGauss->SetLineStyle(5);
-  ppGauss->SetLineColor(kBlue);
-  ppGauss->SetNpx(500);
-  TF1* pLGauss = (TF1*) myfile->Get("pL");
-  pLGauss->SetLineWidth(5);
-  pLGauss->SetLineStyle(5);
-  pLGauss->SetLineColor(30);
-  pLGauss->SetNpx(500);
-  TF1* pS0Gauss = (TF1*) myfile->Get("pS0");
-  pS0Gauss->SetLineWidth(5);
-  pS0Gauss->SetLineStyle(5);
-  pS0Gauss->SetLineColor(38);
-  pS0Gauss->SetNpx(500);
-  TF1* pXimGauss = (TF1*) myfile->Get("pXi");
-  pXimGauss->SetLineWidth(5);
-  pXimGauss->SetLineStyle(5);
-  pXimGauss->SetLineColor(46);
-  pXimGauss->SetNpx(500);
-  TF1* pOmGauss = (TF1*) myfile->Get("pOm");
-  pOmGauss->SetLineWidth(5);
-  pOmGauss->SetLineStyle(5);
-  pOmGauss->SetLineColor(93);
-  pOmGauss->SetNpx(500);
+  CATSparameters* cPars2;
+  cPars2 = new CATSparameters(CATSparameters::tSource, 1, true);
+  cPars2->SetParameter(0, 1.2);
 
-  auto* c1 = new TCanvas("IamBeautiful", "Iambautiful");
-  c1->cd();
-  ppSource->Draw("APL");
-  pLSource->Draw("same");
-  pS0Source->Draw("same");
-  pXiSource->Draw("same");
-  pOmSource->Draw("same");
 
-  ppGauss->Draw("same");
-  pLGauss->Draw("same");
-  pS0Gauss->Draw("same");
-  pXimGauss->Draw("same");
-  pOmGauss->Draw("same");
+  CATS* AB_pp3P2 = new CATS();
+  AB_pp3P2->SetAnaSource(GaussSource, *cPars);
+//  AB_pp3P2->SetAnaSource(CatsSourceForwarder, fppCleverMcLevy, 2);
+  AB_pp3P2->SetAnaSource(0, 1.2);
+  AB_pp3P2->SetAnaSource(1, 2.0);
+  AB_pp3P2->SetEpsilonConv(1e-8);
+  AB_pp3P2->SetEpsilonProp(1e-8);
+  AB_pp3P2->SetUseAnalyticSource(true);
+  AB_pp3P2->SetMomentumDependentSource(false);
+  AB_pp3P2->SetThetaDependentSource(false);
+  AB_pp3P2->SetExcludeFailedBins(false);
+  AB_pp3P2->SetMomBins(200, 0, 400);
+  AB_pp3P2->SetQ1Q2(1);
+  AB_pp3P2->SetPdgId(2212, 2212);
+  AB_pp3P2->SetRedMass(0.5 * massProton);
+  AB_pp3P2->SetNumChannels(1);
+  AB_pp3P2->SetNumPW(0, 1);
+  AB_pp3P2->SetSpin(0, 1);
+  AB_pp3P2->SetChannelWeight(0, 1);
+  AB_pp3P2->SetShortRangePotential(0, 0, fDlmPot, *cPotPars3P2);
+  AB_pp3P2->KillTheCat();
+  CATS* AB_pp3P2Coupled = new CATS();
+  AB_pp3P2Coupled->SetAnaSource(GaussSource, *cPars2);
+//  AB_pp3P2Coupled->SetAnaSource(CatsSourceForwarder, fppCleverMcLevy, 2);
+  AB_pp3P2Coupled->SetAnaSource(0, 1.2);
+  AB_pp3P2Coupled->SetAnaSource(1, 2.0);
+  AB_pp3P2Coupled->SetEpsilonConv(1e-8);
+  AB_pp3P2Coupled->SetEpsilonProp(1e-8);
+  AB_pp3P2Coupled->SetUseAnalyticSource(true);
+  AB_pp3P2Coupled->SetMomentumDependentSource(false);
+  AB_pp3P2Coupled->SetThetaDependentSource(false);
+  AB_pp3P2Coupled->SetExcludeFailedBins(false);
+  AB_pp3P2Coupled->SetMomBins(200, 0, 400);
+  AB_pp3P2Coupled->SetQ1Q2(1);
+  AB_pp3P2Coupled->SetPdgId(2212, 2212);
+  AB_pp3P2Coupled->SetRedMass(0.5 * massProton);
+  AB_pp3P2Coupled->SetNumChannels(1);
+  AB_pp3P2Coupled->SetNumPW(0, 1);
+  AB_pp3P2Coupled->SetSpin(0, 1);
+  AB_pp3P2Coupled->SetChannelWeight(0, 1);
+  AB_pp3P2Coupled->SetShortRangePotential(0, 0, fDlmPot, *cPotPars3P2Coupled);
+  AB_pp3P2Coupled->KillTheCat();
+  TGraph Graph3P2;
+  TGraph Graph3P2Coupled;
+  int dkStar = 2;
+  for (int ik = 0; ik < 200; ik++) {
+    Graph3P2.SetPoint(ik,dkStar*ik,AB_pp3P2->GetCorrFun(ik));
+    Graph3P2Coupled.SetPoint(ik,dkStar*ik,AB_pp3P2Coupled->GetCorrFun(ik));
+  }
+  auto can = new TCanvas();
+  Graph3P2.Draw("APL");
+  Graph3P2Coupled.Draw("PLSame");
+  TFile* out = new TFile("out.root","recreate");
+  out->cd();
+  Graph3P2.SetName("3P2");
+  Graph3P2.Write();
 
-  TLegend* leg = new TLegend(0.4, 0.4, 0.87, 0.87);
-  leg->SetBorderSize(0);
-  leg->SetFillStyle(4000);
-  leg->AddEntry(
-      ppSource,
-      TString::Format(
-          "p#minusp #LT#it{m}_{T}#GT =1.35 GeV/#it{c}^{2} (R_{G,#it{eff}} = %.2f fm)",
-          ppGauss->GetParameter(0)),
-      "l");
-  leg->AddEntry(
-      pLSource,
-      TString::Format(
-          "p#minus#Lambda #LT#it{m}_{T}#GT=1.55 GeV/#it{c}^{2} (R_{G,#it{eff}} = %.2f fm)",
-          pLGauss->GetParameter(0)),
-      "l");
-  leg->AddEntry(
-      pS0Source,
-      TString::Format(
-          "p#minus#Sigma^{0} #LT#it{m}_{T}#GT=2.07 GeV/#it{c}^{2} (R_{G,#it{eff}} = %.2f fm)",
-          pS0Gauss->GetParameter(0)),
-      "l");
-  leg->AddEntry(
-      pXiSource,
-      TString::Format(
-          "p#minus#Xi^{#minus} #LT#it{m}_{T}#GT=1.85 GeV/#it{c}^{2} (R_{G,#it{eff}} = %.2f fm)",
-          pXimGauss->GetParameter(0)),
-      "l");
-  leg->AddEntry(
-      pOmSource,
-      TString::Format(
-          "p#minus#Omega^{#minus} #LT#it{m}_{T}#GT=2.17 GeV/#it{c}^{2} (R_{G,#it{eff}} = %.2f fm)",
-          pOmGauss->GetParameter(0)),
-      "l");
-  leg->Draw("same");
+  Graph3P2Coupled.SetName("3P2Coupled");
+  Graph3P2Coupled.Write();
 
-  TFile* output = new TFile(
-      "~/cernbox/HM13TeV/AnalysisData/ClosePairRej/SelectedPairs/bautiful.root",
-      "RECREATE");
-  c1->SaveAs("Beautiful.png");
-  c1->SaveAs("Beautiful.pdf");
-  output->cd();
-  c1->Write();
-  output->Close();
-  return;
+  can->Write();
+  out->Write();
+  out->Close();
 }
 
 int main(int argc, char *argv[]) {
