e780f8dfaccb75022fb62ebc1c44cbb97fe44a54
bhohlweger <bernhard.hohlweger@cern.ch>
2019-06-22 12:12:02 +0200

    Handeling changes to the execute script

--- GentleKitty/Scripts/ppAnalysis/mTExtractor.C
+++ GentleKitty/Scripts/ppAnalysis/mTExtractor.C
@@ -2,16 +2,31 @@
 #include "VariationmTAnalysis.h"
 #include "TROOT.h"
 int main(int argc, char* argv[])   {
-  gROOT->ProcessLine("gErrorIgnoreLevel = 2001");
+//  gROOT->ProcessLine("gErrorIgnoreLevel = 2001");
   const char* DataDir = argv[1];
   const char* FitDir = argv[2];
-  const int nMTBins = 6;
+  const int nMTBins = 7;
+  TString avgmTFile = TString::Format("%s/AveragemT.root",DataDir);
+  TFile* mTFile = TFile::Open(avgmTFile, "READ");
+  if (!mTFile) {
+    std::cout << "No mT File \n";
+    return -1;
+  }
+  TGraphErrors* avgmT = (TGraphErrors*) mTFile->Get("AveragemT_ppVar0");
+  if(! avgmT) {
+    std::cout << "no average mT file " << std::endl;
+    return -1;
+  }
+
   VariationmTAnalysis* analyser = new VariationmTAnalysis();
   analyser->SetHistName("hCk_RebinnedppVar");
+  analyser->SetmTAverage(avgmT);
   //this implies a folder structure following mTBin_[1,2,3,....]
   for (int imt = 1; imt <= nMTBins; ++imt) {
     TString mTDataDir = Form("%s/mTBin_%u",DataDir,imt);
     analyser->SetSystematic(mTDataDir.Data());
+    TString mTFitDir = Form("%s/mTBin_%u",FitDir,imt);
+    analyser->SetVariation(mTFitDir.Data());
   }
   analyser->MakePlots();
   return 1;
