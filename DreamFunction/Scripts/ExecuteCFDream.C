76ec22a57cda7b3d01dc999bac66fc1e4d2d10f3
bhohlweger <bernhard.hohlweger@cern.ch>
2019-08-27 08:46:58 +0200

    Uses Nano extractor instead of AOD one

--- DreamFunction/Scripts/ExecuteCFDream.C
+++ DreamFunction/Scripts/ExecuteCFDream.C
@@ -1,14 +1,14 @@
-#include "GetCorrelations.C"
+#include "GetCorrelationsNano.C"
 #include "GetCorrelationsBbarB.C"
 #include "METoSEReweighting.C"
 #include "TSystem.h"
 int main(int argc, char* argv[]) {
-  const float fixShift = atof(argv[1]);
-  const char* filename = argv[2];
-  const char* prefix = argv[3];
-  const char* addon = (argv[4]) ? argv[4] : "";
-  GetCorrelations(fixShift,filename, prefix, addon);
-  METoSEReweighting(gSystem->pwd());
+  const char* filename = argv[1];
+  const char* prefix = argv[2];
+  const char* addon = (argv[3]) ? argv[3] : "";
+//  GetCorrelations(fixShift,filename, prefix, addon);
+  GetCorrelations(filename, prefix, addon);
+//  METoSEReweighting(gSystem->pwd());
 
   return 1;
 }
