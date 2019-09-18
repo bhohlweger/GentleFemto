06db5cb56452bdfee8c9b1d118245683f1727543
bhohlweger <bernhard.hohlweger@cern.ch>
2019-09-18 12:42:21 +0200

    Can now be used for minimum bias, moves radius plot to dedicated script

--- GentleKitty/Scripts/pLAnalysis/mTExtractorAddPot.C
+++ GentleKitty/Scripts/pLAnalysis/mTExtractorAddPot.C
@@ -5,17 +5,13 @@
 #include "TStyle.h"
 #include "TLatex.h"
 
-void DrawRadiusVsMT(const char* filepp, const char* filepL,
-                    const char* sourceName);
 
 int main(int argc, char* argv[]) {
 //  gROOT->ProcessLine("gErrorIgnoreLevel = 2001");
   const char* DataDir = argv[1];
   const char* FitDirResonance = argv[2];
-  const char* FitDirGauss = argv[3];
-  const char* ppFileResonance = (argv[4]) ? argv[4] : "";
-  const char* ppFileGauss = (argv[5]) ? argv[5] : "";
-  const int nMTBins = 6;
+  int SourceOption = atoi(argv[3]);
+  const int nMTBins = atoi(argv[4]);
   TString avgmTFile = TString::Format("%s/AveragemT.root", DataDir);
   TFile* mTFile = TFile::Open(avgmTFile, "READ");
   if (!mTFile) {
@@ -28,14 +24,22 @@ int main(int argc, char* argv[]) {
     mTFile->ls();
     return -1;
   }
-
-  VariationmTAnalysis* analyser = new VariationmTAnalysis(2, 42, 162);
+  int nVariations = 0;
+  if (nMTBins == 6) {
+    nVariations = 42;
+  } else if (nMTBins == 3) {
+    nVariations = 31;
+  }
+  VariationmTAnalysis* analyser = new VariationmTAnalysis(1, nVariations, 162);
   analyser->SetHistName("hCk_RebinnedpLVar");
   analyser->SetFileName("OutFileVarpL.root");
   analyser->SetmTAverage(avgmT);
   analyser->SetLegData("p-#Lambda #oplus #bar{p}-#bar{#Lambda}", "fpe");
-  analyser->SetLegModel("Fit Gaussian + Resonance Source", "f", 1, 3252);
-  analyser->SetLegModel("Fit Gaussian Source", "f", 1, 3225);
+  if (SourceOption == 0) {
+    analyser->SetLegModel("Fit Gauss", "f", 1, -1);
+  } else if (SourceOption == 1) {
+    analyser->SetLegModel("Fit Gauss+Resonance", "f", 1, -1);
+  }
   analyser->SetTextXMin(0.35);
   analyser->SetPlottingRange(0, 230);
   //this implies a folder structure following mTBin_[1,2,3,....]
@@ -47,111 +51,18 @@ int main(int argc, char* argv[]) {
     analyser->SetSystematic(mTDataDir.Data());
     TString mTFitDirResonance = Form("%s/mTBin_%u/", FitDirResonance, imt);
     analyser->SetVariation(mTFitDirResonance.Data(), 0);
-    TString mTFitDirGauss = Form("%s/mTBin_%u/", FitDirGauss, imt);
-    analyser->SetVariation(mTFitDirGauss.Data(), 1);
   }
-  std::vector<float> mTBins = { 1.08, 1.26, 1.32, 1.44, 1.65, 1.9, 4.5 };
-  analyser->SetmTBins(mTBins);
-  analyser->MakeCFPlotsSingleBand();
-  analyser->StoreRadvsmT("RadpLvsmTGaussReso.root", 0);
-  analyser->StoreRadvsmT("RadpLvsmTGauss.root", 1);
 
-  if (ppFileResonance != "" && ppFileGauss != "") {
-    std::cout << "pp file Name: " << ppFileGauss << " and " << ppFileResonance
-              << " passed\n";
-    DrawRadiusVsMT(ppFileResonance, "RadpLvsmTGaussReso.root",
-                   "Gauss+Resonances");
-    DrawRadiusVsMT(ppFileGauss, "RadpLvsmTGauss.root", "Gauss");
+  if (nMTBins == 6) {
+    std::vector<float> mTBins = { 1.02, 1.14, 1.2, 1.26, 1.38, 1.56, 1.86, 4.5 };
+    analyser->SetmTBins(mTBins);
+  } else if (nMTBins == 3) {
+    std::vector<float> mTBins = { 1.08, 1.3,1.5, 4.5 };
+    analyser->SetmTBins(mTBins);
   }
+  analyser->MakeCFPlotsSingleBand();
+  analyser->StoreRadvsmT("RadpLvsmT.root", 0);
 
   return 1;
 }
 
-void DrawRadiusVsMT(const char* filepp, const char* filepL,
-                    const char* sourceName) {
-  DreamPlot::SetStyle();
-  TFile* ppFile = TFile::Open(filepp, "read");
-  TFile* pLFile = TFile::Open(filepL, "read");
-  TGraphErrors* mTppSys = (TGraphErrors*) ppFile->Get("mTRadiusSyst");
-  TGraphErrors* mTppStat = (TGraphErrors*) ppFile->Get("mTRadiusStat");
-  TGraphErrors* mTpLSys = (TGraphErrors*) pLFile->Get("mTRadiusSyst");
-  TGraphErrors* mTpLStat = (TGraphErrors*) pLFile->Get("mTRadiusStat");
-  for (int iBin = 0; iBin < mTppSys->GetN(); iBin++) {
-    mTppSys->SetPointError(iBin, 0.4 * mTppSys->GetErrorX(iBin),
-                           mTppSys->GetErrorY(iBin));
-  }
-  for (int iBin = 0; iBin < mTpLSys->GetN(); iBin++) {
-    mTpLSys->SetPointError(iBin, 0.4 * mTpLSys->GetErrorX(iBin),
-                           mTpLSys->GetErrorY(iBin));
-  }
-  TFile* out = TFile::Open(Form("%s.root",sourceName), "recreate");
-  out->cd();
-  auto c4 = new TCanvas("c8", "c8", 1200, 800);
-  c4->cd();
-  TLegend* leg = new TLegend(0.55, 0.5, 0.85, 0.7);
-  leg->SetFillStyle(4000);
-  leg->SetTextFont(43);
-  leg->SetTextSizePixels(32);
-  mTpLSys->SetPoint(mTpLSys->GetN(), 0.95, 0.5);
-  mTpLSys->SetPointError(mTpLSys->GetN(), 0., 0.);
-
-  mTpLSys->SetPoint(mTpLSys->GetN(), 2.7, 1.3);
-  mTpLSys->SetPointError(mTpLSys->GetN(), 0., 0.);
-
-  mTpLSys->SetTitle("; < m_{T} >  (MeV/#it{c}^{2}); r_{Core} (fm)");
-  mTpLSys->GetXaxis()->SetTitleSize(40);
-  mTpLSys->GetYaxis()->SetTitleSize(40);
-  mTpLSys->GetXaxis()->SetTitleOffset(1.4);
-  mTpLSys->GetYaxis()->SetTitleOffset(1.4);
-  mTpLSys->GetXaxis()->SetLabelSize(40);
-  mTpLSys->GetYaxis()->SetLabelSize(40);
-  mTpLSys->GetXaxis()->SetLabelOffset(.02);
-  mTpLSys->GetYaxis()->SetLabelOffset(.02);
-  mTpLSys->GetXaxis()->SetRangeUser(0.95, 2.7);
-
-  if (strcmp(sourceName, "Gauss") == 0) {
-    mTpLSys->GetYaxis()->SetRangeUser(0.8, 1.9);
-  } else {
-    mTpLSys->GetYaxis()->SetRangeUser(0.45, 1.4);
-  }
-
-  mTpLSys->SetFillColorAlpha(kRed - 7, 0.7);
-  mTpLSys->Draw("2AP");
-
-  mTpLStat->SetMarkerColor(kRed + 1);
-  mTpLStat->SetLineColor(kRed + 1);
-  mTpLStat->SetFillColor(kRed + 1);
-  mTpLStat->SetLineWidth(1);
-  mTpLStat->SetMarkerStyle(23);
-  mTpLStat->SetMarkerSize(1.2);
-  mTpLStat->Draw("pez same");
-
-  mTppSys->SetFillColorAlpha(kBlue - 7, 0.7);
-  mTppSys->Draw("2PSAME");
-
-  mTppStat->SetMarkerColor(kBlue + 2);
-  mTppStat->SetLineColor(kBlue + 2);
-  mTppStat->SetFillColor(kBlue + 2);
-  mTppStat->SetLineWidth(1);
-  mTppStat->SetMarkerStyle(20);
-  mTppStat->SetMarkerSize(1.2);
-  mTppStat->Draw("pez same");
-
-  leg->AddEntry(mTppStat, "p#minus p", "pl");
-  leg->AddEntry(mTpLStat, "p#minus #Lambda", "pl");
-  TLatex BeamText;
-  BeamText.SetTextSize(gStyle->GetTextSize() * .85);
-  BeamText.SetNDC(kTRUE);
-  BeamText.DrawLatex(0.55, 0.83,
-                     Form("ALICE %s #sqrt{#it{s}} = %i TeV", "pp", (int) 13));
-  BeamText.DrawLatex(0.55, 0.77, "High Mult. (0-0.072% INEL)");
-  BeamText.DrawLatex(0.55, 0.72, Form("%s", sourceName));
-
-  leg->Draw("same");
-  c4->SaveAs(Form("%s/mTvsRad%s.pdf", gSystem->pwd(), sourceName));
-  c4->Write();
-  out->Write();
-  out->Close();
-  ppFile->Close();
-  pLFile->Close();
-}
--- GentleKitty/Scripts/ppAnalysis/mTExtractor.C
+++ GentleKitty/Scripts/ppAnalysis/mTExtractor.C
@@ -4,8 +4,8 @@ int main(int argc, char* argv[]) {
 //  gROOT->ProcessLine("gErrorIgnoreLevel = 2001");
   const char* DataDir = argv[1];
   const char* FitDirResonance = argv[2];
-  const char* FitDirGauss = argv[3];
-  const int nMTBins = 7;
+  int SourceOption = atoi(argv[3]);
+  const int nMTBins = atoi(argv[4]);
   TString avgmTFile = TString::Format("%s/AveragemT.root", DataDir);
   TFile* mTFile = TFile::Open(avgmTFile, "READ");
   if (!mTFile) {
@@ -17,14 +17,24 @@ int main(int argc, char* argv[]) {
     std::cout << "no average mT file " << std::endl;
     return -1;
   }
-
-  VariationmTAnalysis* analyser = new VariationmTAnalysis(2, 26, 81);
+  int nVariations = 0;
+  if (nMTBins == 7) {
+    nVariations = 26;
+  } else if (nMTBins == 3) {
+    nVariations = 24;
+  }
+  VariationmTAnalysis* analyser = new VariationmTAnalysis(1, nVariations, 81);
   analyser->SetHistName("hCk_RebinnedppVar");
+//  analyser->SetHistName("hCk_FixShiftedppVar");
   analyser->SetFileName("OutFileVarpp.root");
   analyser->SetmTAverage(avgmT);
   analyser->SetLegData("p-p #oplus #bar{p}-#bar{p}", "fpe");
-  analyser->SetLegModel("Fit Gaussian + Resonance", "f", 2, 3252);
-  analyser->SetLegModel("Fit Gaussian", "f", 2, 3225);
+  if (SourceOption == 0) {
+    analyser->SetLegModel("Fit Gauss", "f", 2, -1);
+  } else if (SourceOption == 1) {
+    analyser->SetLegModel("Fit Gauss+Resonance", "f", 2, -1);
+  }
+//  analyser->SetLegModel("Fit Gaussian", "f", 2, 3225);
   //this implies a folder structure following mTBin_[1,2,3,....]
   for (int imt = 1; imt <= nMTBins; ++imt) {
     std::cout << "========================== \n";
@@ -34,13 +44,18 @@ int main(int argc, char* argv[]) {
     analyser->SetSystematic(mTDataDir.Data());
     TString mTFitDirResonance = Form("%s/mTBin_%u", FitDirResonance, imt);
     analyser->SetVariation(mTFitDirResonance.Data(), 0);
-    TString mTFitDirGauss = Form("%s/mTBin_%u", FitDirGauss, imt);
-    analyser->SetVariation(mTFitDirGauss.Data(), 1);
+//    TString mTFitDirGauss = Form("%s/mTBin_%u", FitDirGauss, imt);
+//    analyser->SetVariation(mTFitDirGauss.Data(), 1);
   }
-  std::vector<float> mTBins = { 1.02, 1.14, 1.2, 1.26, 1.38, 1.56, 1.86, 4.5 };
-  analyser->SetmTBins(mTBins);
+  if (nMTBins == 7) {
+    std::vector<float> mTBins = { 1.02, 1.14, 1.2, 1.26, 1.38, 1.56, 1.86, 4.5 };
+    analyser->SetmTBins(mTBins);
+  } else if (nMTBins == 3) {
+    std::vector<float> mTBins = { 1.02, 1.14, 1.26, 4.5 };
+    analyser->SetmTBins(mTBins);
+  }
   analyser->MakeCFPlotsSingleBand();
-  analyser->StoreRadvsmT("RadppvsmTGaussReso.root",0);
-  analyser->StoreRadvsmT("RadppvsmTGauss.root",1);
+  analyser->StoreRadvsmT("RadppvsmT.root",0);
+//  analyser->StoreRadvsmT("RadppvsmTGauss.root",1);
   return 1;
 }
