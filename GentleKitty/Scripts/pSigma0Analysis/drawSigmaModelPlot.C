cfcdbeca0c8a6dedd50e33a9ed54ce7f4a6ef633
Andreas Mathis <andreas.mathis@cern.ch>
2019-10-11 10:14:28 +0200

    Sigma0 model plotter in line with latest style and more radii

--- GentleKitty/Scripts/pSigma0Analysis/drawSigmaModelPlot.C
+++ GentleKitty/Scripts/pSigma0Analysis/drawSigmaModelPlot.C
@@ -19,6 +19,8 @@
 #include "TStyle.h"
 #include "TLatex.h"
 
+void DrawSigma(const float r);
+
 /// =====================================================================================
 void FillCkGraph(DLM_Ck *ck, TGraph *gr) {
   for (unsigned int i = 0; i < ck->GetNbins(); ++i) {
@@ -37,36 +39,47 @@ void FillWaveGraph(CATS &kitty, TGraph *gr) {
 
 /// =====================================================================================
 int main(int argc, char* argv[]) {
+  DrawSigma(1.25);
+  DrawSigma(2.);
+  DrawSigma(3.);
+  DrawSigma(4);
+}
+
+void DrawSigma(const float r) {
   DreamPlot::SetStyle();
   double* radius = new double[1];
-  radius[0] = 1.154;
+  radius[0] = r;
   int momBins = 80;
   int kmin = -2.49;
   int kmax = 397.51;
 
-  const int colorfss2 = kRed + 1;
+  const int colorfss2 = kRed + 2;
+  const float alphafss2 = 0.5;
   const int colorchiEFT = kAzure;
+  const float alphaEFT = 0.5;
   const int colorESC16 = kGreen + 2;
+  const float alphaESC16 = 0.6;
   const int colorNSC97f = kOrange - 3;
+  const float alphaNSC97f = 0.5;
   const int lineWidth = 3;
 
   /// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   /// Proton-Lambda correlation functions
   auto grLambdafss2 = new TGraph();
   grLambdafss2->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grLambdafss2, 20, colorfss2);
+  DreamPlot::SetStyleGraph(grLambdafss2, 20, colorfss2, alphafss2);
   grLambdafss2->SetLineWidth(lineWidth);
   auto grLambdachiEFT = new TGraph();
   grLambdachiEFT->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grLambdachiEFT, 20, colorchiEFT);
+  DreamPlot::SetStyleGraph(grLambdachiEFT, 20, colorchiEFT, alphaEFT);
   grLambdachiEFT->SetLineWidth(lineWidth);
   auto grLambdaESC16 = new TGraph();
   grLambdaESC16->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grLambdaESC16, 20, colorESC16);
+  DreamPlot::SetStyleGraph(grLambdaESC16, 20, colorESC16, alphaESC16);
   grLambdaESC16->SetLineWidth(lineWidth);
   auto grLambdaNSC97f = new TGraph();
   grLambdaNSC97f->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grLambdaNSC97f, 20, colorNSC97f);
+  DreamPlot::SetStyleGraph(grLambdaNSC97f, 20, colorNSC97f, alphaNSC97f);
   grLambdaNSC97f->SetLineWidth(lineWidth);
 
   /// chiEFT
@@ -117,19 +130,19 @@ int main(int argc, char* argv[]) {
   /// Proton-Sigma0 correlation functions
   auto grSigma0fss2 = new TGraph();
   grSigma0fss2->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grSigma0fss2, 20, colorfss2);
+  DreamPlot::SetStyleGraph(grSigma0fss2, 20, colorfss2, alphafss2);
   grSigma0fss2->SetLineWidth(lineWidth);
   auto grSigma0chiEFT = new TGraph();
   grSigma0chiEFT->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grSigma0chiEFT, 20, colorchiEFT);
+  DreamPlot::SetStyleGraph(grSigma0chiEFT, 20, colorchiEFT, alphaEFT);
   grSigma0chiEFT->SetLineWidth(lineWidth);
   auto grSigma0ESC16 = new TGraph();
   grSigma0ESC16->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grSigma0ESC16, 20, colorESC16);
+  DreamPlot::SetStyleGraph(grSigma0ESC16, 20, colorESC16, alphaESC16);
   grSigma0ESC16->SetLineWidth(lineWidth);
   auto grSigma0NSC97f = new TGraph();
   grSigma0NSC97f->SetTitle(";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)");
-  DreamPlot::SetStyleGraph(grSigma0NSC97f, 20, colorNSC97f);
+  DreamPlot::SetStyleGraph(grSigma0NSC97f, 20, colorNSC97f, alphaNSC97f);
   grSigma0NSC97f->SetLineWidth(lineWidth);
 
   /// chiEFT
@@ -172,18 +185,29 @@ int main(int argc, char* argv[]) {
 
   /// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   /// Plotting
-  gStyle->SetHatchesSpacing(0.75);
-  const float kmaxdraw = 300;
-  const float right = 0.03;
+  const float kmaxdraw = 200;
+  const float right = 0.04;
   const float top = 0.025;
 
+  float yminSigma = 0.25;
+  const float ymaxSigma = 2.5;
+
+  auto dummyHist = new TH1F("dummyHist", ";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)", 100, 0, kmaxdraw);
+  DreamPlot::SetStyleHisto(dummyHist, 20, colorNSC97f);
+  auto dummyHistLambda = new TH1F("dummyHistLambda", ";#it{k}* (MeV/#it{c}); #it{C}(#it{k}*)", 100, 0, kmaxdraw);
+  DreamPlot::SetStyleHisto(dummyHistLambda, 20, colorNSC97f);
+
+  TLatex pair;
+  pair.SetTextSize(gStyle->GetTextSize() * 1.25);
+  pair.SetNDC(kTRUE);
 
   auto d = new TCanvas("CFpLambda", "CFpLambda", 0, 0, 650, 550);
   d->SetRightMargin(right);
   d->SetTopMargin(top);
-  grLambdachiEFT->Draw("AL3");
-  grLambdachiEFT->GetXaxis()->SetRangeUser(0, kmaxdraw);
-  grLambdachiEFT->GetYaxis()->SetRangeUser(0.8, 3.5);
+  dummyHistLambda->Draw();
+  dummyHistLambda->GetYaxis()->SetRangeUser(0.8, 3.25);
+  dummyHistLambda->GetXaxis()->SetNdivisions(504);
+  grLambdachiEFT->Draw("L3same");
   grLambdaESC16->Draw("L3same");
   grLambdaNSC97f->Draw("L3same");
   grLambdafss2->Draw("L3same");
@@ -202,23 +226,30 @@ int main(int argc, char* argv[]) {
   leg->AddEntry(grSigma0ESC16, "ESC16", "l");
   leg->AddEntry(grSigma0NSC97f, "NSC97f", "l");
   leg->Draw("same");
-  d->Print("Lambda.pdf");
+  pair.DrawLatex(0.275, 0.885, "p#minus#kern[-0.75]{ }#Lambda");
+  d->Print(Form("Lambda_%.0f-%i.pdf", radius[0], int((radius[0] - int(radius[0]))*100.f)));
 
   auto c = new TCanvas("CFpSigma", "CFpSigma", 0, 0, 650, 550);
   c->SetRightMargin(right);
   c->SetTopMargin(top);
-  grSigma0chiEFT->Draw("AL3");
-  grSigma0chiEFT->GetXaxis()->SetRangeUser(0, kmaxdraw);
-  grSigma0chiEFT->GetYaxis()->SetRangeUser(0.75, 2.75);
+  dummyHist->Draw();
+  dummyHist->GetYaxis()->SetRangeUser(yminSigma, ymaxSigma);
+  dummyHist->GetXaxis()->SetNdivisions(504);
+  grSigma0chiEFT->Draw("L3same");
   grSigma0ESC16->Draw("L3same");
   grSigma0NSC97f->Draw("L3same");
   grSigma0fss2->Draw("L3same");
   leg->Draw("same");
-  c->Print("Sigma0.pdf");
+  pair.DrawLatex(0.275, 0.885, "p#minus#kern[-0.75]{ }#Sigma^{0}");
+  c->Print(Form("Sigma0_%.0f-%i.pdf", radius[0], int((radius[0] - int(radius[0]))*100.f)));
   xmin = 0.33;
   xwidth = 0.6;
-  ymin = 0.31;
-  ywidth = 0.39;
+  ymin = 0.325;
+  ywidth = 0.375;
+
+  yminSigma = 0.75;
+  dummyHist->GetYaxis()->SetRangeUser(yminSigma, ymaxSigma);
+  dummyHist->GetYaxis()->SetNdivisions(504);
   TPad *inset_pad = new TPad("insert", "insertPad", xmin, ymin, xmin+xwidth, ymin+ywidth);
   inset_pad->SetTopMargin(0.01);
   inset_pad->SetRightMargin(0.05);
@@ -227,26 +258,46 @@ int main(int argc, char* argv[]) {
   inset_pad->SetFillStyle(4000);
   inset_pad->Draw();
   inset_pad->cd();
-  grLambdachiEFT->Draw("AL3");
-  grLambdachiEFT->GetXaxis()->SetRangeUser(0, kmaxdraw);
-  grLambdachiEFT->GetYaxis()->SetRangeUser(0.8, 3.5);
-  grLambdachiEFT->GetYaxis()->SetNdivisions(203);
-  grLambdachiEFT->GetXaxis()->SetNdivisions(204);
-  grLambdachiEFT->GetXaxis()->SetTitleOffset(2.5);
-  grLambdachiEFT->GetYaxis()->SetTitleOffset(1.1);
+  dummyHistLambda->Draw();
+  dummyHistLambda->GetYaxis()->SetRangeUser(0.9, 3.2);
+  dummyHistLambda->GetYaxis()->SetNdivisions(503);
+  dummyHistLambda->GetXaxis()->SetTitleOffset(2.5);
+  dummyHistLambda->GetYaxis()->SetTitleOffset(1.1);
   const float scaleSize = 0.85;
-  grLambdachiEFT->GetXaxis()->SetTitleSize(scaleSize * grLambdachiEFT->GetXaxis()->GetTitleSize());
-  grLambdachiEFT->GetYaxis()->SetTitleSize(scaleSize * grLambdachiEFT->GetYaxis()->GetTitleSize());
-  grLambdachiEFT->GetXaxis()->SetLabelSize(scaleSize * grLambdachiEFT->GetXaxis()->GetLabelSize());
-  grLambdachiEFT->GetYaxis()->SetLabelSize(scaleSize * grLambdachiEFT->GetYaxis()->GetLabelSize());
+  dummyHistLambda->GetXaxis()->SetTitleSize(scaleSize * dummyHistLambda->GetXaxis()->GetTitleSize());
+  dummyHistLambda->GetYaxis()->SetTitleSize(scaleSize * dummyHistLambda->GetYaxis()->GetTitleSize());
+  dummyHistLambda->GetXaxis()->SetLabelSize(scaleSize * dummyHistLambda->GetXaxis()->GetLabelSize());
+  dummyHistLambda->GetYaxis()->SetLabelSize(scaleSize * dummyHistLambda->GetYaxis()->GetLabelSize());
+  grLambdachiEFT->Draw("L3same");
   grLambdaESC16->Draw("L3same");
   grLambdaNSC97f->Draw("L3same");
   grLambdafss2->Draw("L3same");
   c->cd();
-  TLatex pair;
-  pair.SetTextSize(gStyle->GetTextSize() * 1.25);
-  pair.SetNDC(kTRUE);
-  pair.DrawLatex(0.275, 0.885, "p#minus#kern[-0.75]{ }#Sigma^{0}");
   pair.DrawLatex(0.775, 0.625, "p#minus#kern[-1.]{ }#Lambda");
-  c->Print("Sigma0-Lambda.pdf");
+  c->Print(Form("Sigma0-Lambda_%.0f-%i.pdf", radius[0], int((radius[0] - int(radius[0]))*100.f)));
+
+  delete inset_pad;
+  delete c;
+  delete leg;
+  delete d;
+  delete Sigma0fss2;
+  delete Sigma0NSC97f;
+  delete Sigma0ESC16;
+  delete Sigma0chiEFT;
+  delete tidy;
+  delete grSigma0NSC97f;
+  delete grSigma0ESC16;
+  delete grSigma0chiEFT;
+  delete grSigma0fss2;
+  delete LambdaNSC97f;
+  delete LambdaESC16;
+  delete Lambdafss2;
+  delete LambdaChiEFTHaidenbauer;
+  delete grLambdaNSC97f;
+  delete grLambdaESC16;
+  delete grLambdafss2;
+  delete grLambdachiEFT;
+  delete dummyHist;
+  delete dummyHistLambda;
+  delete[] radius;
 }
