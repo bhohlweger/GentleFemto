c2be0d05ac6493e9260ef82573a006f6d49ac265
bhohlweger <bernhard.hohlweger@cern.ch>
2019-10-28 17:50:59 +0100

    Adds functionality for a quick CF comparison to models and full Unfolding in place

--- FemtoGami/Scripts/XiGami.C
+++ FemtoGami/Scripts/XiGami.C
@@ -5,43 +5,90 @@
 #include "SideBandFit.h"
 
 #include "TF1.h"
+#include "TSystem.h"
+
 #include "DLM_CkDecomposition.h"
 
 static double cutOff = 1000;  // at this value the calculation and doing of the cf stops
+TFile* QAOutput = nullptr;
 
 double SetupLambdaPars(LambdaGami* XiGami, double ProVar, double OmegaVar,
                        double Xi1530Var);
 TH1F* BaseLine(TH1F* dataCF);
 TH1F* XimSideband(LambdaGami* XiGami, TH1F* dataCF, unsigned int varSideNorm);
 TH1F* Xim1530FeedDown(LambdaGami* XiGami, TH1F* dataCF);
+void StoreModels(TH1F* unfoldedGenuine, TFile* QAOutput);
 int main(int argc, char *argv[]) {
   const char* fileName = argv[1];
-  const char* HistName = argv[2];
-  TFile* inFile = TFile::Open(fileName, "READ");
-  if (!inFile) {
-    Error("Main", "No Input file\n");
-    return -1;
-  }
+  const char* prefix = argv[2];
+
   LambdaGami* XiGami = new LambdaGami();
-  //read histogram
-  TH1F* CFMeasured = (TH1F*) inFile->FindObjectAny(HistName);
-  if (!CFMeasured) {
-    Error("Main", "No Input Histo found\n");
-    inFile->ls();
-    return -2;
-  }
-  //Get rid of the bassline or vaseline
-  TH1F* CFBLFree = BaseLine(CFMeasured);
-  //Setup the Lambda Parameters
+  MomentumGami* MomGami = new MomentumGami(0.5);
+
+  auto CATSinput = new CATSInput();
+  CATSinput->SetNormalization(0.500, 0.700);
+  CATSinput->SetFixedkStarMinBin(true, 0.);
+  const int rebin = 5;
+
+  TString CalibBaseDir = "~/cernbox/SystematicsAndCalib/ppRun2_HM/";
+  TString SigmaFileName = "Sample6_MeV_compact.root";
+  CATSinput->SetCalibBaseDir(CalibBaseDir.Data());
+  CATSinput->SetSigmaFileName(SigmaFileName.Data());
+  CATSinput->ReadSigmaFile();
+  TH2F* momReso = (TH2F*)CATSinput->GetSigmaFile(3)->Clone("MomResolution");
+  momReso->Rebin2D(2);
+  MomGami->SetResolution(momReso, 1000);
+  CATSinput->SetMomentumGami(MomGami);
+
+  ReadDreamFile* DreamFile = new ReadDreamFile(6, 6);
+  DreamFile->SetAnalysisFile(fileName, prefix, "0");
+
+  DreamDist* pXi = DreamFile->GetPairDistributions(0, 4, "");
+  DreamDist* ApAXi = DreamFile->GetPairDistributions(1, 5, "");
+  DreamCF* CFpXiDef = CATSinput->ObtainCFSyst(rebin, "pXiVar0", pXi, ApAXi);
+  TH1F* CFMeasured = CFpXiDef->FindCorrelationFunction(
+      "hCk_ReweightedpXiVar0MeV_1");
+  CFMeasured = (TH1F*)CFMeasured->Clone("InputCF");
+  QAOutput = TFile::Open(
+      TString::Format("%s/debug.root", gSystem->pwd()).Data(), "recreate");
+//  CFMeasured->SetDirectory(0);
+  XiGami->StoreStatErr(CFMeasured);
+  QAOutput->cd();
+  CFMeasured->Write();
+
   double lamGenuine = SetupLambdaPars(XiGami, 1., 1., 1.);
+  //Loop over variations, but set pointer for default to be passed on ?
+
   //Get rid of the sidebands
   TH1F* unfoldedSideBand = XimSideband(XiGami, CFMeasured, 1);
-  //Unfold for momentum resolution
-  TH1F* unfoldedMomRes = unfoldedSideBand;
+  QAOutput->cd();
+  XiGami->AddStatErr(unfoldedSideBand);
+  unfoldedSideBand->Write();
+
+  //Get rid of the bassline or vaseline
+  TH1F* CFBLFree = BaseLine(unfoldedSideBand);
+  QAOutput->cd();
+  XiGami->AddStatErr(CFBLFree);
+  CFBLFree->Write();
+
+  //Setup the Lambda Parameters
+
   //Get rid of the p-Xim1530 smeared
-  TH1F* unfoldedFeedDown = Xim1530FeedDown(XiGami, unfoldedMomRes);
+  TH1F* unfoldedFeedDown = Xim1530FeedDown(XiGami, CFBLFree);
+  QAOutput->cd();
+  XiGami->AddStatErr(unfoldedFeedDown);
+  unfoldedFeedDown->Write();
+
   //Unfold to the genuine CF
   TH1F* unfoldedGenuine = XiGami->UnfoldGenuine(unfoldedFeedDown, lamGenuine);
+  QAOutput->cd();
+  XiGami->AddStatErr(unfoldedGenuine);
+  unfoldedGenuine->Write();
+
+//  StoreModels(unfoldedGenuine, QAOutput);
+  CFpXiDef->WriteOutput(QAOutput, false);
+  QAOutput->Close();
+
   return 0;
 }
 
@@ -51,6 +98,8 @@ TH1F* BaseLine(TH1F* dataCF) {
   TF1* funct_1 = new TF1("myPol1", "pol1", 0, cutOff);
   dataCF->Fit(funct_1, "SN", "", 400, 900);
   BaseLineFree->Divide(funct_1);
+  QAOutput->cd();
+  funct_1->Write();
   return BaseLineFree;
 }
 
@@ -93,6 +142,8 @@ TH1F* XimSideband(LambdaGami* XiGami, TH1F* dataCF, unsigned int varSideNorm) {
         iBims, SideBandFit::ParameterizationROOT(mom, SideBandPars));
   }
   delete mom;
+  QAOutput->cd();
+  sideBand->Write();
   return XiGami->UnfoldResidual(dataCF, sideBand, XiGami->GetLamdaPar(0));
 }
 
@@ -127,7 +178,6 @@ TH1F* Xim1530FeedDown(LambdaGami* XiGami, TH1F* dataCF) {
 
   AB_pXim1530.SetAnaSource(0, 0.92);  //for now 1.2 fm .. ADJUST!
   AB_pXim1530.KillTheCat();
-
   DLM_Ck* ck = new DLM_Ck(AB_pXim1530.GetNumSourcePars(), 0, AB_pXim1530);
   DLM_CkDecomposition dec = DLM_CkDecomposition("dummy", 0, *ck, nullptr);
   DLM_ResponseMatrix* resp = new DLM_ResponseMatrix(*ck, NULL,
@@ -136,8 +186,7 @@ TH1F* Xim1530FeedDown(LambdaGami* XiGami, TH1F* dataCF) {
 //  smeared = nullptr;
   DLM_Histo<double>*CkSmeared = new DLM_Histo<double>();
   CkSmeared->SetUp(1);
-  CkSmeared->SetUp(0, ck->GetNbins(0), ck->GetLowEdge(0),
-                   ck->GetUpEdge(0));
+  CkSmeared->SetUp(0, ck->GetNbins(0), ck->GetLowEdge(0), ck->GetUpEdge(0));
   CkSmeared->Initialize();
   CkSmeared->SetBinContentAll(0);
   CkSmeared->SetBinErrorAll(0);
@@ -147,9 +196,14 @@ TH1F* Xim1530FeedDown(LambdaGami* XiGami, TH1F* dataCF) {
   TH1F* pXim1530Converted = tidy->Convert2LesserOf2Evils(CkSmeared, dataCF);
   pXim1530Converted->SetName("pXim1530Converted");
   pXim1530Converted->SetTitle("pXim1530Converted");
-
+  for (int iBin = pXim1530Converted->FindBin(500);
+      iBin < pXim1530Converted->GetNbinsX() + 1; ++iBin) {
+    pXim1530Converted->SetBinContent(iBin, 1);
+  }
   delete ck;
   delete resp;
+  QAOutput->cd();
+  pXim1530Converted->Write();
   return XiGami->UnfoldResidual(dataCF, pXim1530Converted,
                                 XiGami->GetLamdaPar(1));
 }
@@ -219,4 +273,54 @@ double SetupLambdaPars(LambdaGami* XiGami, double ProVar, double OmegaVar,
                             0, 2));
   return lamPar.GetLambdaParam(CATSLambdaParam::Primary,
                                CATSLambdaParam::Primary);
+}
+
+void StoreModels(TH1F* unfoldedGenuine, TFile* QAOutput) {
+  int nBins = unfoldedGenuine->FindBin(520);
+  double xmin = 0;
+  double xmax = 520;
+  TidyCats* tidy = new TidyCats();
+  TidyCats::Sources TheSource = TidyCats::sGaussian;
+  float ppRadii[3];
+  ppRadii[0] = 0.76;
+  ppRadii[1] = 0.79;
+  ppRadii[2] = 0.82;
+
+  CATS Coulomb;
+
+  tidy->GetCatsProtonXiMinus(&Coulomb, nBins, xmin, xmax, TheSource,
+                             TidyCats::pCoulomb, 0);
+  Coulomb.SetAnaSource(0, ppRadii[1]);
+  Coulomb.KillTheCat();
+
+  CATS HAL;
+  tidy->GetCatsProtonXiMinus(&HAL, nBins, xmin, xmax, TheSource,
+                             TidyCats::pHALQCD, 12);
+  HAL.SetAnaSource(0, ppRadii[1]);
+  HAL.KillTheCat();
+
+  CATS ESC16;
+  tidy->GetCatsProtonXiMinus(&ESC16, nBins, xmin, xmax, TheSource,
+                             TidyCats::pRikkenPot, 0);
+  ESC16.SetAnaSource(0, ppRadii[1]);
+  ESC16.KillTheCat();
+
+  TGraph* cou = new TGraph();
+  TGraph* hal = new TGraph();
+  TGraph* esc = new TGraph();
+
+  for (auto it = 0 ; it < unfoldedGenuine->FindBin(xmax); ++it) {
+    double kStar = unfoldedGenuine->GetBinCenter(it+1);
+    cou->SetPoint(it , kStar, Coulomb.GetCorrFun(it));
+    hal->SetPoint(it , kStar, HAL.GetCorrFun(it));
+    esc->SetPoint(it , kStar, ESC16.GetCorrFun(it));
+  }
+  QAOutput->cd();
+  cou->SetName("coulomb");
+  cou->Write();
+  hal->SetName("halqcd");
+  hal->Write();
+  esc->SetName("esc");
+  esc->Write();
+  return;
 }
