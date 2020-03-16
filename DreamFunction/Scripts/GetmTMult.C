f97e788d118d901c9c5c49f304fe52d9623e1b88
bhohlweger <bernhard.hohlweger@cern.ch>
2020-03-16 11:14:35 +0100

    Script to read the mT Mult binned histograms and get the CFs

---
+++ DreamFunction/Scripts/GetmTMult.C
@@ -0,0 +1,25 @@
+#include "TROOT.h"
+#include "ReadDreamFile.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TSystem.h"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  const char* filename = argv[1];
+  const char* prefix = argv[2];
+  const char* addon = (argv[3]) ? argv[3] : "";
+  //gStyle->SetOptStat(0);
+  ReadDreamFile* DreamFile = new ReadDreamFile(4, 4);
+  DreamFile->ReadmTMultHistos(filename, prefix, addon,3);
+  /*
+  DreamCF* CF_pp = new DreamCF();
+  DreamPair* pp = new DreamPair("Part", 0.24, 0.34);
+  DreamPair* ApAp = new DreamPair("AntiPart", 0.24, 0.34);
+  pp->SetPair(DreamFile->GetPairDistributions(0, 0, ""));
+  ApAp->SetPair(DreamFile->GetPairDistributions(1, 1, ""));
+  */
+}
+
+
+
